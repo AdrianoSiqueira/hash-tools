1ec704fd684c9a001a4df490d8d7e12571fe5be5
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2021-02-18 21:29:01 -0300

    New class to dynamically switch between operating modes.

---
+++ src/listener/PaneGeneratorExpansionListener.java
@@ -0,0 +1,48 @@
+package listener;
+
+import controller.AppUI;
+import handler.HashCheckerHandler;
+import handler.HashGeneratorHandler;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import language.LanguageManager;
+import main.Main;
+
+public class PaneGeneratorExpansionListener implements ChangeListener<Boolean> {
+
+    private final AppUI controller;
+
+    public PaneGeneratorExpansionListener(AppUI controller) {
+        this.controller = controller;
+    }
+
+    @Override
+    public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+        Main.resize(500, false);
+
+        controller.setDoneDetails(0.0);
+
+        controller.getField2()
+                  .setEditable(!newValue);
+        controller.getField2()
+                  .clear();
+
+        if (newValue) {
+            controller.getButtonRun()
+                      .setOnAction(new HashGeneratorHandler(controller));
+            controller.getLabelTitle()
+                      .setText(LanguageManager.get("Generate.hash.sum"));
+            controller.getLabel2()
+                      .setText(LanguageManager.get("Output"));
+            controller.getItemSelectNone()
+                      .fire();
+        } else {
+            controller.getButtonRun()
+                      .setOnAction(new HashCheckerHandler(controller));
+            controller.getLabelTitle()
+                      .setText(LanguageManager.get("Check.hash.sum"));
+            controller.getLabel2()
+                      .setText(LanguageManager.get("Hash"));
+        }
+    }
+}
