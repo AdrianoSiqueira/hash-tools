c5e2b3265a9b4aa7ca24f8660d614c6601022d34
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2021-06-15 20:00:14 -0300

    Pre-preparation to add support for command-line mode of operation.

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,21 +6,36 @@ import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import language.LanguageManager;
 
 import java.io.IOException;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
-public class Main extends Application {
+public class Main
+        extends Application {
 
     private static Stage        stage;
     private static HostServices hostServices;
 
+
     public static void main(String[] args) {
-        launch(args);
+        if (args.length == 0) {
+            // Run GUI mode
+            launch();
+        } else if (args[0].equalsIgnoreCase("--help")) {
+            // Show CLI help content
+            throw new UnsupportedOperationException(LanguageManager.get("Resource.not.implemented.yet."));
+        } else if (args[0].equalsIgnoreCase("--check")) {
+            // Run check sequence.
+            throw new UnsupportedOperationException(LanguageManager.get("Resource.not.implemented.yet."));
+        } else if (args[0].equalsIgnoreCase("--generate")) {
+            // Run generation sequence.
+            throw new UnsupportedOperationException(LanguageManager.get("Resource.not.implemented.yet."));
+        }
     }
+
 
     public static HostServices getHostService() {
         return hostServices;
@@ -47,22 +62,27 @@ public class Main extends Application {
         }).start();
     }
 
+
+    @Override
     public void start(Stage stage)
     throws IOException {
+        Class<Main> thisClass = Main.class;
+
         Main.stage        = stage;
         Main.hostServices = this.getHostServices();
 
         FXMLLoader loader = new FXMLLoader(
-                Main.class.getResource("/gui/AppUI.fxml"),
+                thisClass.getResource("/gui/AppUI.fxml"),
                 LanguageManager.getBundle()
         );
 
         Parent root = loader.load();
-        root.getStylesheets().add("/css/AppUI.css");
+        root.getStylesheets()
+            .add(Objects.requireNonNull(thisClass.getResource("/css/AppUI.css"))
+                        .toString());
 
         stage.setTitle(LanguageManager.get("HashTools"));
         stage.setScene(new Scene(root));
-        stage.getIcons().add(new Image("/icon/sha256.png"));
         stage.show();
     }
 }
