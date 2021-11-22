b9c3c4331c348a54a4ae72857716e4d92e554d52
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2021-11-21 21:59:54 -0300

    Improved design, now allows changing the language.

---
+++ src/hashtools/core/language/LanguageManager.java
@@ -0,0 +1,28 @@
+package hashtools.core.language;
+
+import java.util.Locale;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+/**
+ * <p>Handles the management of multiple languages.</p>
+ *
+ * @author Adriano Siqueira
+ * @version 2.0.0
+ * @since 1.0.0
+ */
+public class LanguageManager {
+
+    private static final String         NAME   = "hashtools.core.language.Language";
+    private static       ResourceBundle bundle = ResourceBundle.getBundle(NAME);
+
+
+    public static String get(String key) {
+        return bundle.getString(key);
+    }
+
+    public static void setLanguage(Locale locale) {
+        Optional.ofNullable(locale)
+                .ifPresent(l -> bundle = ResourceBundle.getBundle(NAME, l));
+    }
+}
