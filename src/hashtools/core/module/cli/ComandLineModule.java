2c4df4b81fa58a487980ac9db16fd9418844ca60
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2021-11-23 22:16:54 -0300

    Code refactored for better organization.

--- src/hashtools/core/module/cli/ComandLineModule.java
+++ src/hashtools/core/module/cli/ComandLineModule.java
@@ -4,47 +4,107 @@ import hashtools.core.module.checker.CheckerModule;
 import hashtools.core.module.generator.GeneratorModule;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 /**
- * <p>Allows execution from the command line.</p>
+ * <p>
+ * Allows execution from the command line.
+ * </p>
+ *
+ * TODO Translate all the interaction and Exception messages.
  *
  * @author Adriano Siqueira
- * @version 1.0.0
+ * @version 1.0.1
  * @since 2.0.0
  */
 public class ComandLineModule implements Runnable {
 
     private final String[] arguments;
 
+
+    /**
+     * <p>
+     * Creates an instance of {@link ComandLineModule} class.
+     * </p>
+     *
+     * @param arguments Arguments used to run the application.
+     *
+     * @since 1.0.0
+     */
     public ComandLineModule(String[] arguments) {
         this.arguments = Objects.requireNonNull(arguments);
     }
 
 
+    /**
+     * <p>
+     * Run the check sequence.
+     * </p>
+     *
+     * @throws IllegalArgumentException If there are any errors in the arguments.
+     * @since 1.0.1
+     */
+    private void runCheckSequence()
+    throws IllegalArgumentException {
+        if (arguments.length != 3)
+            throw new IllegalArgumentException("Wrong number of arguments.");
+
+
+        System.out.println("HashTools" + System.lineSeparator() +
+                           ">> Running: Check Module");
+
+        double percentage = new CheckerModule(arguments[1], arguments[2])
+                .call()
+                .getReliabilityPercentage();
+
+        System.out.println(">> Execution done." + System.lineSeparator() +
+                           "   Reliability: " + percentage + " %");
+    }
+
+    /**
+     * <p>
+     * Run the generation sequence.
+     * </p>
+     *
+     * @throws IllegalArgumentException If there are any errors in the arguments.
+     * @since 1.0.1
+     */
+    private void runGenerationSequence()
+    throws IllegalArgumentException {
+        if (arguments.length < 4)
+            throw new IllegalArgumentException("Wrong number of arguments..");
+
+
+        System.out.println("HashTools" + System.lineSeparator() +
+                           ">> Running: Generation Module");
+
+        String[] algorithms = Arrays.copyOfRange(arguments, 3, arguments.length);
+
+        new GeneratorModule(arguments[1], arguments[2], List.of(algorithms))
+                .call();
+
+        System.out.println(">> Execution done.");
+    }
+
+
+    /**
+     * <p>
+     * Determines the execution mode by analyzing the given arguments.
+     * </p>
+     *
+     * @throws IllegalArgumentException If there are any errors in the arguments.
+     * @since 1.0.0
+     */
     @Override
-    public void run() {
+    public void run()
+    throws IllegalArgumentException {
         if (arguments[0].equalsIgnoreCase("--check")) {
-            if (arguments.length != 3) throw new IllegalArgumentException();
-
-            // TODO Remove this line
-            System.out.println("Checking");
-
-            double score = new CheckerModule(arguments[1], arguments[2])
-                    .call()
-                    .getReliabilityPercentage();
-
-            System.out.println("Total score: " + score);
+            runCheckSequence();
         } else if (arguments[0].equalsIgnoreCase("--generate")) {
-            if (arguments.length < 4) throw new IllegalArgumentException();
-
-            // TODO Remove this line
-            System.out.println("Generating");
-
-            String[] algorithms = Arrays.copyOfRange(arguments, 3, arguments.length);
-
-            new GeneratorModule(arguments[1], arguments[2], algorithms)
-                    .call();
+            runGenerationSequence();
+        } else {
+            throw new IllegalArgumentException("Invalid execution module: '" + arguments[0] + "'");
         }
     }
 }
