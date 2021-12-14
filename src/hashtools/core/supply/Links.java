ecf0e10e735ed41f6a14edb9cfc48945320e942e
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2021-12-14 19:47:37 -0300

    Renamed options with better names and added link to LinkedIn.

--- src/hashtools/core/supply/Links.java
+++ src/hashtools/core/supply/Links.java
@@ -4,14 +4,15 @@ package hashtools.core.supply;
  * <p>Dedicated to storing some useful links.</p>
  *
  * @author Adriano Siqueir
- * @version 1.0.0
+ * @version 1.1.0
  * @since 1.0.0
  */
 public enum Links {
 
-    DEVELOPER_PROFILE("https://github.com/AdrianoSiqueira"),
-    ONLINE_DOCUMENTATION("https://github.com/AdrianoSiqueira/HashTools/wiki"),
-    PROJECT("https://github.com/AdrianoSiqueira/HashTools");
+    DEVELOPER_GITHUB_PROFILE("https://github.com/AdrianoSiqueira"),
+    DEVELOPER_LINKEDIN_PROFILE("https://linkedin.com/in/asadrianosiqueira/"),
+    APPLICATION_ONLINE_DOCUMENTATION("https://github.com/AdrianoSiqueira/HashTools/wiki"),
+    GITHUB_PROJECT("https://github.com/AdrianoSiqueira/HashTools");
 
 
     private final String url;
