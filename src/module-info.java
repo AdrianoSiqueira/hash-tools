b6e604ad9fbd2588fa771963fb9bf44a26ee7cca
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2021-12-24 19:58:54 -0300

    Added entry for 'Manual' screen.

--- src/module-info.java
+++ src/module-info.java
@@ -7,8 +7,10 @@ module HashTools {
 
     opens hashtools.gui.screen.about;
     opens hashtools.gui.screen.app;
+    opens hashtools.gui.screen.manual;
 
     exports hashtools.main;
     exports hashtools.gui.screen.about;
     exports hashtools.gui.screen.app;
+    exports hashtools.gui.screen.manual;
 }
