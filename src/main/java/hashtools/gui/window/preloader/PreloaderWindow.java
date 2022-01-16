f8d8584379f181420501661f30ba16476d5f9c0c
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2022-01-16 20:05:02 -0300

    Added preloader screen.

---
+++ src/main/java/hashtools/gui/window/preloader/PreloaderWindow.java
@@ -0,0 +1,58 @@
+package hashtools.gui.window.preloader;
+
+import javafx.application.Platform;
+import javafx.application.Preloader;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * <p>
+ * Window that will hold the preloader screen.
+ * </p>
+ *
+ * @author Adriano Siqueira
+ * @version 1.0.0
+ * @since 2.0.0
+ */
+public class PreloaderWindow extends Preloader {
+
+    private Stage stage;
+
+
+    private void closeAfterDelay() {
+        new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException ignored) {}
+
+            Platform.runLater(stage::close);
+        }).start();
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    private Scene createScene() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Preloader.fxml"));
+
+        return new Scene(root);
+    }
+
+
+    @Override
+    public void handleStateChangeNotification(StateChangeNotification info) {
+        if (info.getType() == StateChangeNotification.Type.BEFORE_START) closeAfterDelay();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        this.stage = stage;
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setScene(createScene());
+        stage.show();
+    }
+}
