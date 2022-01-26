97d41fd4672b4b6c3c59e0a4b39ba483902b8e5f
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2022-01-25 21:52:09 -0300

    Added checker screen controller.

---
+++ src/main/java/hashtools/gui/screen/checker/CheckerController.java
@@ -0,0 +1,44 @@
+package hashtools.gui.screen.checker;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CheckerController implements Initializable {
+
+    @FXML private VBox       paneRoot;
+    @FXML private GridPane   paneForm;
+    @FXML private TitledPane titledPane;
+    @FXML private ScrollPane scrollPane;
+    @FXML private StackPane  paneResult;
+
+    @FXML private TextField  fieldInput;
+    @FXML private TextField  fieldOfficial;
+
+    @FXML private Button     buttonOpenInput;
+    @FXML private Button     buttonOpenOfficial;
+    @FXML private Button     buttonCheck;
+
+    @FXML private Label      labelResult;
+
+
+    @FXML private void openInputFile(ActionEvent event){}
+    @FXML private void openOfficialFile(ActionEvent event){}
+    @FXML private void runCheckingModule(ActionEvent event){}
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+}
