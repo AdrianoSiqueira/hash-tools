f6994c87d0b035c64af5f6f0223a6016881c2606
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2022-01-26 21:10:39 -0300

    Removed unnecessary methods.

--- src/main/java/hashtools/gui/screen/checker/CheckerController.java
+++ src/main/java/hashtools/gui/screen/checker/CheckerController.java
@@ -24,7 +24,6 @@ import java.net.URL;
 import java.util.Arrays;
 import java.util.ResourceBundle;
 import java.util.StringJoiner;
-import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
@@ -110,12 +109,6 @@ public class CheckerController implements Initializable {
         fieldOfficial.setText("/home/adriano/IdeaProjects/HashTools/temp-files/light-sample.txt");
     }
 
-    private void process() {
-        try {
-            TimeUnit.SECONDS.sleep(2);
-        } catch (InterruptedException ignored) {}
-    }
-
     private void runCheckerModule() {
         SampleList sampleList = new CheckerModule(
                 fieldInput.getText(),
@@ -151,42 +144,6 @@ public class CheckerController implements Initializable {
 
     private void stopSplash() {
         Platform.runLater(() -> currentScene.setRoot(currentRoot));
-    }
-
-    private void writeResult() {
-        labelResult.setText("""
-                             ------------------------------------------------------------------------------------------------------------------------------------------------------
-                              Algorithm: MD-5
-                               Official: 904d96a9d2c9393d24c289d8ea79cd47
-                             Calculated: 904d96a9d2c9393d24c289d8ea79cd47
-                                 Status: Safe
-                             ------------------------------------------------------------------------------------------------------------------------------------------------------
-                              Algorithm: SHA-1
-                               Official: 470881e437ae957a49451f8e50ad162d7bc5cda9
-                             Calculated: 470881e437ae957a49451f8e50ad162d7bc5cda9
-                                 Status: Safe
-                             ------------------------------------------------------------------------------------------------------------------------------------------------------
-                              Algorithm: SHA-224
-                               Official: 5ed2a377fd4855e9d6a0fa1a0ad456e9c7453f69e780c3eebceec1ef
-                             Calculated: 5ed2a377fd4855e9d6a0fa1a0ad456e9c7453f69e780c3eebceec1ef
-                                 Status: Safe
-                             ------------------------------------------------------------------------------------------------------------------------------------------------------
-                              Algorithm: SHA-256
-                               Official: d448623bc40f18b241328a046e447bfea4ab260ac753ef20a49f342f5fa6f23f
-                             Calculated: d448623bc40f18b241328a046e447bfea4ab260ac753ef20a49f342f5fa6f23f
-                                 Status: Safe
-                             ------------------------------------------------------------------------------------------------------------------------------------------------------
-                              Algorithm: SHA-384
-                               Official: 4b0151650b54c59f82f23cf966d7104c18317f3bc2426c0b74f7db108251d403f28f225136d506954e65cc3ae6030e1e
-                             Calculated: 4b0151650b54c59f82f23cf966d7104c18317f3bc2426c0b74f7db108251d403f28f225136d506954e65cc3ae6030e1e
-                                 Status: Safe
-                             ------------------------------------------------------------------------------------------------------------------------------------------------------
-                              Algorithm: SHA-512
-                               Official: befb819daf5ad9907814ef693396cfadbec82af978b4c427d0410a4d258f9bfc0e92b244fdc741554f23b86a25218a9938ce77110516c3e4b040eb6643af4771
-                             Calculated: befb819daf5ad9907814ef693396cfadbec82af978b4c427d0410a4d258f9bfc0e92b244fdc741554f23b86a25218a9938ce77110516c3e4b040eb6643af4771
-                                 Status: Safe
-                            ------------------------------------------------------------------------------------------------------------------------------------------------------
-                             """);
     }
 
 
