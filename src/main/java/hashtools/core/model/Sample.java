0d55bfae561aff1ad0e9aabf0f9fefd09efa2022
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2022-02-02 19:58:41 -0300

    Revert "Changed internal structure to use string instead of object."
    This reverts commit aecfbc789753dc3e97026225da59265c94983f49.

--- src/main/java/hashtools/core/model/Sample.java
+++ src/main/java/hashtools/core/model/Sample.java
@@ -1,7 +1,6 @@
 package hashtools.core.model;
 
 import aslib.security.SHAType;
-import hashtools.core.service.FileService;
 import hashtools.core.util.FilePathDetector;
 
 import java.nio.file.Path;
@@ -15,9 +14,6 @@ import java.nio.file.Path;
  */
 public class Sample {
 
-    private String  input;
-    private boolean usingFile;
-
     private Object  object;
     private boolean usingFileAsObject;
 
@@ -27,30 +23,14 @@ public class Sample {
     private Result  result;
 
 
-    public Object getInput() {
-        return usingFile
-               ? Path.of(input)
-               : input;
+    public Sample() {
     }
 
-    public Sample setInput(String input) {
-        if (input == null) return this;
 
-        this.input = input;
-        this.usingFile = FileService.stringIsFilePath(input);
-        return this;
-    }
-
-    public boolean isUsingFile() {
-        return usingFile;
-    }
-
-    @Deprecated(forRemoval = true)
     public Object getObject() {
         return object;
     }
 
-    @Deprecated(forRemoval = true)
     public Sample setObject(Object object) {
         if (object instanceof Path path) {
             this.object = path;
@@ -66,7 +46,6 @@ public class Sample {
         return this;
     }
 
-    @Deprecated(forRemoval = true)
     public boolean isUsingFileAsObject() {
         return usingFileAsObject;
     }
@@ -111,8 +90,8 @@ public class Sample {
     @Override
     public String toString() {
         return "Sample{" +
-               "input=" + input +
-               ", usingFile=" + usingFile +
+               "object=" + object +
+               ", usingFileAsObject=" + usingFileAsObject +
                ", algorithm=" + algorithm +
                ", officialHash='" + officialHash + '\'' +
                ", calculatedHash='" + calculatedHash + '\'' +
