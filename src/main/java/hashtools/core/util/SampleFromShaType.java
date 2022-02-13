47daac006b4a5a9fe4f1a30dc7ac892569f0a9c0
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2022-02-13 20:49:16 -0300

    Migrated from SHAType to HashAlgorithm.

--- src/main/java/hashtools/core/util/SampleFromShaType.java
+++ src/main/java/hashtools/core/util/SampleFromShaType.java
@@ -1,26 +1,26 @@
 package hashtools.core.util;
 
-import aslib.security.SHAType;
+import hashtools.core.model.HashAlgorithm;
 import hashtools.core.model.Sample;
 
 import java.util.Optional;
 import java.util.function.Function;
 
 /**
- * <p>Class designed to create an object of type Sample from a {@link SHAType}.</p>
+ * <p>Class designed to create an object of type Sample from a {@link HashAlgorithm}.</p>
  *
  * @author Adriano Siqueira
  * @version 1.0.0
  * @since 2.0.0
  */
-public class SampleFromShaType implements Function<SHAType, Sample> {
+public class SampleFromShaType implements Function<HashAlgorithm, Sample> {
 
     /**
-     * <p>Creates an object of type Sample from a {@link SHAType} object.</p>
+     * <p>Creates an object of type Sample from a {@link HashAlgorithm} object.</p>
      *
      * <p>The object will already have the algorithm attribute initialized.</p>
      *
-     * <p>If the {@link SHAType} is null, a null object is returned.</p>
+     * <p>If the {@link HashAlgorithm} is null, a null object is returned.</p>
      *
      * @param shaType Used to create the object.
      *
@@ -29,7 +29,7 @@ public class SampleFromShaType implements Function<SHAType, Sample> {
      * @since 1.0.0
      */
     @Override
-    public Sample apply(SHAType shaType) {
+    public Sample apply(HashAlgorithm shaType) {
         return Optional.ofNullable(shaType)
                        .map(algorithm -> new Sample().setAlgorithm(algorithm))
                        .orElse(null);
