550203e1855e79d2060e12faafe265939e48779e
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2022-02-28 20:48:40 -0300

    Remove unnecessary item.

--- src/main/java/hashtools/core/model/Result.java
+++ src/main/java/hashtools/core/model/Result.java
@@ -1,62 +1,18 @@
 package hashtools.core.model;
 
-import hashtools.core.language.LanguageManager;
+import lombok.Getter;
 
-/**
- * <p>It represents the possible results of the analysis.</p>
- *
- * @author Adriano Siqueira
- * @version 1.0.0
- * @since 2.0.0
- */
+@Getter
 public enum Result {
 
-    /**
-     * <p>Used when hashes do not match.</p>
-     *
-     * @since 1.0.0
-     */
-    DANGEROUS(0.0, "Dangerous"),
-
-    /**
-     * <p>Used when the hashes match perfectly.</p>
-     *
-     * @since 1.0.0
-     */
-    SAFE(1.0, "Safe"),
-
-    /**
-     * <p>Used when hashes match regardless of the case.</p>
-     *
-     * @since 1.0.0
-     */
-    UNSURE(0.5, "Unsure");
+    SAFE("Safe"),
+    UNSAFE("Unsafe");
 
 
-    private double score;
     private String text;
 
 
-    Result(double score, String text) {
-        this.score = score;
+    Result(String text) {
         this.text = text;
-    }
-
-
-    public double getScore() {
-        return score;
-    }
-
-    public String getText() {
-        return LanguageManager.get(text);
-    }
-
-
-    @Override
-    public String toString() {
-        return "Result{" +
-               "score=" + score +
-               ", text='" + text + '\'' +
-               '}';
     }
 }
