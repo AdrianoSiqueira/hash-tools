1c7d4c1b92ee96badd05c3ae8df8ed9b0f990c42
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2022-02-28 20:43:34 -0300

    Remove unnecessary methods.

--- src/main/java/hashtools/core/model/SampleList.java
+++ src/main/java/hashtools/core/model/SampleList.java
@@ -5,20 +5,10 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
-/**
- * <p>Collects samples into a list to be processed. In addition to storing
- * general reliability metrics.</p>
- *
- * @author Adriano Siqueira
- * @version 1.0.0
- * @since 2.0.0
- */
 public class SampleList {
 
     private List<Sample> samples;
-
-    private double reliabilityPercentage;
-    private double maxPossibleScore;
+    private double       reliabilityPercentage;
 
 
     public SampleList() {
@@ -33,7 +23,6 @@ public class SampleList {
 
     public void setSamples(List<Sample> samples) {
         this.samples = samples;
-        calculateMaxPossibleScore();
     }
 
     public double getReliabilityPercentage() {
@@ -44,27 +33,15 @@ public class SampleList {
         this.reliabilityPercentage = reliabilityPercentage;
     }
 
-    public double getMaxPossibleScore() {
-        return maxPossibleScore;
-    }
-
 
     public void add(Sample... samples) {
         Arrays.stream(samples)
               .filter(Objects::nonNull)
               .forEach(this.samples::add);
-
-        calculateMaxPossibleScore();
     }
 
     public void clear() {
         samples.clear();
         reliabilityPercentage = 0.0;
-        maxPossibleScore = 0.0;
-    }
-
-
-    private void calculateMaxPossibleScore() {
-        maxPossibleScore = samples.size() * Result.SAFE.getScore();
     }
 }
