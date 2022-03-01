d3ce34ebb44b173c941c46d4cec0193f431a1e3a
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2022-02-28 21:30:59 -0300

    Add method to create algorithms list.

--- src/main/java/hashtools/core/module/cli/ComandLineModule.java
+++ src/main/java/hashtools/core/module/cli/ComandLineModule.java
@@ -1,12 +1,16 @@
 package hashtools.core.module.cli;
 
 import hashtools.core.language.LanguageManager;
+import hashtools.core.model.HashAlgorithm;
 import hashtools.core.module.checker.CheckerModule;
 import hashtools.core.module.generator.GeneratorModule;
+import hashtools.core.service.HashAlgorithmService;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 public class ComandLineModule implements Runnable {
 
@@ -17,6 +21,14 @@ public class ComandLineModule implements Runnable {
         this.arguments = Objects.requireNonNull(arguments);
     }
 
+
+    private List<HashAlgorithm> prepareHashAlgorithms() {
+        return Stream.of(Arrays.copyOfRange(arguments, 3, arguments.length))
+                     .map(new HashAlgorithmService()::searchByName)
+                     .filter(Optional::isPresent)
+                     .map(Optional::get)
+                     .toList();
+    }
 
     private void runCheckSequence()
     throws IllegalArgumentException {
@@ -44,10 +56,11 @@ public class ComandLineModule implements Runnable {
         System.out.println("HashTools" + System.lineSeparator() +
                            ">> " + LanguageManager.get("Running") + ": " + LanguageManager.get("Generation.Module."));
 
-        String[] algorithms = Arrays.copyOfRange(arguments, 3, arguments.length);
-
-        new GeneratorModule(arguments[1], arguments[2], List.of(algorithms))
-                .call();
+        new GeneratorModule(
+                arguments[1],
+                arguments[2],
+                prepareHashAlgorithms()
+        ).call();
 
         System.out.println(">> " + LanguageManager.get("Execution.done."));
     }
