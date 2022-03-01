9450492d929b2c56bc5c6df10f126338d17b5c8a
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2022-02-28 21:09:31 -0300

    Remove javadoc.

--- src/main/java/hashtools/core/module/generator/GeneratorModule.java
+++ src/main/java/hashtools/core/module/generator/GeneratorModule.java
@@ -14,15 +14,6 @@ import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.Callable;
 
-/**
- * <p>
- * Class responsible for executing the generation module.
- * </p>
- *
- * @author Adriano Siqueira
- * @version 1.0.2
- * @since 2.0.0
- */
 public class GeneratorModule implements Callable<SampleList> {
 
     private final Object  inputObject;
@@ -30,40 +21,13 @@ public class GeneratorModule implements Callable<SampleList> {
     private final List<?> algorithms;
 
 
-    /**
-     * <p>
-     * Creates an instance of {@link GeneratorModule} class.
-     * </p>
-     *
-     * <p>
-     * The input object can be a file, a path to a file, or any string.
-     * </p>
-     *
-     * <p>
-     * The destination file will be erased to contain only the new data. It will
-     * be created if it doesn't exist.
-     * </p>
-     *
-     * @param inputObject Object used to generate hashes. Not null.
-     * @param destination File where generated hashes will be saved.
-     * @param algorithms  Algorithms used to generate the hashes.
-     *
-     * @since 1.0.0
-     */
     public GeneratorModule(Object inputObject, Object destination, List<?> algorithms) {
         this.inputObject = inputObject;
         this.destination = Path.of(destination.toString());
         this.algorithms = algorithms;
     }
 
-    /**
-     * <p>
-     * Clears the content of destination file. The file will be created if
-     * necessary.
-     * </p>
-     *
-     * @since 1.0.0
-     */
+
     private void clearDestination() {
         try {
             Files.writeString(destination, "", StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE);
@@ -72,38 +36,11 @@ public class GeneratorModule implements Callable<SampleList> {
         }
     }
 
-    /**
-     * <p>
-     * Sets the test object to the sample.
-     * </p>
-     *
-     * <p>
-     * This method is designed to be used within a stream.
-     * </p>
-     *
-     * @param sample Sample where the object will be set.
-     *
-     * @since 1.0.0
-     */
     private void setObjectToSample(Sample sample) {
         sample.setObject(inputObject);
     }
 
 
-    /**
-     * <p>
-     * Executes the entire sequence of generating the hashes.
-     * </p>
-     *
-     * <p>
-     * This method generates the hashes and writes the data to the destination
-     * file.
-     * </p>
-     *
-     * @return A {@link SampleList} object with the generated data.
-     *
-     * @since 1.0.0
-     */
     @Override
     public SampleList call() {
         if (algorithms.isEmpty()) return null;
