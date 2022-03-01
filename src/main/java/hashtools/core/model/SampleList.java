372a377943b61b97c74ff5f1926f46ccbaeab2d8
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2022-03-01 11:57:12 -0300

    Implement lombok.

--- src/main/java/hashtools/core/model/SampleList.java
+++ src/main/java/hashtools/core/model/SampleList.java
@@ -1,10 +1,15 @@
 package hashtools.core.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
+@Getter
+@Setter
 public class SampleList {
 
     private List<Sample> samples;
@@ -14,23 +19,6 @@ public class SampleList {
     public SampleList() {
         this.samples = new ArrayList<>();
         this.reliabilityPercentage = 0.0;
-    }
-
-
-    public List<Sample> getSamples() {
-        return samples;
-    }
-
-    public void setSamples(List<Sample> samples) {
-        this.samples = samples;
-    }
-
-    public double getReliabilityPercentage() {
-        return reliabilityPercentage;
-    }
-
-    public void setReliabilityPercentage(double reliabilityPercentage) {
-        this.reliabilityPercentage = reliabilityPercentage;
     }
 
 
