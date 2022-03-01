f24896e3d09ca2b8ec7214e227b690a15e17fb9a
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2022-03-01 18:24:44 -0300

    Implement dedicated classes to generate hashes.

--- src/main/java/hashtools/core/service/HashService.java
+++ src/main/java/hashtools/core/service/HashService.java
@@ -2,13 +2,10 @@ package hashtools.core.service;
 
 import hashtools.core.model.HashAlgorithm;
 import hashtools.core.model.Sample;
+import hashtools.core.module.generator.FileHashGenerator;
+import hashtools.core.module.generator.StringHashGenerator;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.nio.file.Path;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class HashService {
 
@@ -23,47 +20,10 @@ public class HashService {
     }
 
     public String generate(HashAlgorithm algorithm, String string) {
-        byte[] stringBytes = string.getBytes();
-
-        MessageDigest messageDigest = getMessageDigest(algorithm);
-        byte[]        bytes         = messageDigest.digest(stringBytes);
-
-        return hexBytesToString(bytes);
+        return new StringHashGenerator().generate(algorithm, string);
     }
 
     public String generate(HashAlgorithm algorithm, Path path) {
-        MessageDigest messageDigest = getMessageDigest(algorithm);
-
-        try (BufferedInputStream stream = new BufferedInputStream(new FileInputStream(path.toFile()))) {
-            byte[] buffer = new byte[1024];
-            int    amountRead;
-
-            while ((amountRead = stream.read(buffer)) > -1) {
-                messageDigest.update(buffer, 0, amountRead);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        byte[] bytes = messageDigest.digest();
-        return hexBytesToString(bytes);
-    }
-
-    private MessageDigest getMessageDigest(HashAlgorithm algorithm) {
-        try {
-            return MessageDigest.getInstance(algorithm.getName());
-        } catch (NoSuchAlgorithmException e) {
-            throw new IllegalArgumentException(e.getMessage());
-        }
-    }
-
-    private String hexBytesToString(byte[] bytes) {
-        StringBuilder builder = new StringBuilder();
-
-        for (byte b : bytes) {
-            builder.append(String.format("%02x", b));
-        }
-
-        return builder.toString();
+        return new FileHashGenerator().generate(algorithm, path);
     }
 }
