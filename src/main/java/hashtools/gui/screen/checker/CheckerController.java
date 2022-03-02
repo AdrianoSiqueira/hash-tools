78cba64f3a62f9ad54aa437a5efcaf1af1557a58
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2022-03-01 22:42:57 -0300

    Remove unused methods.

--- src/main/java/hashtools/gui/screen/checker/CheckerController.java
+++ src/main/java/hashtools/gui/screen/checker/CheckerController.java
@@ -3,7 +3,6 @@ package hashtools.gui.screen.checker;
 import hashtools.core.consumer.CheckerGUISampleContainerConsumer;
 import hashtools.core.language.LanguageManager;
 import hashtools.core.model.FileExtension;
-import hashtools.core.model.HashAlgorithm;
 import hashtools.core.model.SampleContainer;
 import hashtools.core.module.checker.CheckerModule;
 import hashtools.core.service.FileService;
@@ -36,10 +35,8 @@ import javafx.scene.layout.VBox;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Path;
-import java.util.Arrays;
 import java.util.Optional;
 import java.util.ResourceBundle;
-import java.util.StringJoiner;
 
 /**
  * <p>
@@ -101,13 +98,6 @@ public class CheckerController implements Initializable {
         event.acceptTransferModes(transferModes);
     }
 
-    private int calculateIdealTabSize(String... strings) {
-        return Arrays.stream(strings)
-                     .map(String::length)
-                     .reduce(Math::max)
-                     .orElse(0);
-    }
-
     private boolean clearIsNotNecessary() {
         return !needClearResult;
     }
@@ -123,39 +113,6 @@ public class CheckerController implements Initializable {
     @FXML
     private void clearResultWhenTextFieldContentChange(ObservableValue<String> observable, String oldValue, String newValue) {
         clearResult();
-    }
-
-    private String formatResult(SampleContainer sampleContainer) {
-        String s1 = LanguageManager.get("Algorithm");
-        String s2 = LanguageManager.get("Calculated");
-        String s3 = LanguageManager.get("Official");
-        String s4 = LanguageManager.get("Result");
-
-        int idealSize = calculateIdealTabSize(s1, s2, s3, s4);
-
-        String algorithm  = String.format("%" + idealSize + "s: ", s1);
-        String calculated = String.format("%" + idealSize + "s: ", s2);
-        String official   = String.format("%" + idealSize + "s: ", s3);
-        String result     = String.format("%" + idealSize + "s: ", s4);
-
-        StringJoiner joiner = new StringJoiner("-".repeat(idealSize + HashAlgorithm.SHA512.getLength() + 2),
-                                               "-".repeat(idealSize + HashAlgorithm.SHA512.getLength() + 2),
-                                               "-".repeat(idealSize + HashAlgorithm.SHA512.getLength() + 2));
-
-        sampleContainer.getSamples()
-                       .forEach(s -> {
-                           String ls = System.lineSeparator();
-
-                           String content = ls +
-                                            algorithm + s.getAlgorithm().getName() + ls +
-                                            official + s.getOfficialHash() + ls +
-                                            calculated + s.getCalculatedHash() + ls +
-                                            result + s.getResult().getText() + ls;
-
-                           joiner.add(content);
-                       });
-
-        return joiner.toString();
     }
 
     @FXML
@@ -237,14 +194,6 @@ public class CheckerController implements Initializable {
 
     private void stopSplash() {
         Platform.runLater(() -> currentScene.setRoot(currentRoot));
-    }
-
-    private void writeResult(SampleContainer sampleContainer) {
-        String result = formatResult(sampleContainer);
-        labelResult.setText(result);
-
-        progressBar.setProgress(sampleContainer.getReliabilityPercentage() / 100);
-        needClearResult = true;
     }
 
 
