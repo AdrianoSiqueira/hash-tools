9fa9272b49e80e9ffae4983c33426ad9efc52ddb
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2022-03-07 08:57:24 -0300

    Delegate formatResult method implementation to GeneratorCLISampleContainerConsumer class.

--- src/main/java/hashtools/core/consumer/GeneratorGUISampleContainerConsumer.java
+++ src/main/java/hashtools/core/consumer/GeneratorGUISampleContainerConsumer.java
@@ -1,8 +1,6 @@
 package hashtools.core.consumer;
 
-import hashtools.core.model.Sample;
 import hashtools.core.model.SampleContainer;
-import hashtools.core.service.FileService;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 
@@ -16,13 +14,6 @@ public class GeneratorGUISampleContainerConsumer implements SampleContainerConsu
     private final Path outputFile;
 
 
-    private String getFileName(Sample sample) {
-        return new FileService().stringIsFilePath(sample.getInputData())
-               ? Path.of(sample.getInputData()).getFileName().toString()
-               : sample.getInputData();
-    }
-
-
     @SneakyThrows
     @Override
     public void accept(SampleContainer sampleContainer) {
@@ -32,15 +23,6 @@ public class GeneratorGUISampleContainerConsumer implements SampleContainerConsu
 
     @Override
     public String formatResult(SampleContainer sampleContainer) {
-        StringBuilder builder  = new StringBuilder();
-        String        fileName = getFileName(sampleContainer.getSamples().get(0));
-
-        sampleContainer.getSamples()
-                       .forEach(sample -> builder.append(sample.getCalculatedHash())
-                                                 .append("  ")
-                                                 .append(fileName)
-                                                 .append(System.lineSeparator()));
-
-        return builder.toString();
+        return new GeneratorCLISampleContainerConsumer().formatResult(sampleContainer);
     }
 }
