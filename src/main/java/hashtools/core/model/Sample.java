2a7c35622b1368cd798c6345d0887fdb9a17d958
adriano <as.adriano.siqueira@gmail.com>
2022-12-31 20:38:43 -0300

    refactor: Make input object store both string and path.

--- src/main/java/hashtools/core/model/Sample.java
+++ src/main/java/hashtools/core/model/Sample.java
@@ -1,14 +1,58 @@
 package hashtools.core.model;
 
-import lombok.Data;
+import java.nio.file.Path;
 
-@Data
 public class Sample {
 
-    private String inputData;
+    private Object input;
 
-    private HashAlgorithm algorithm;
-    private String        officialHash;
-    private String        calculatedHash;
-    private Result        result;
+    private String algorithm;
+    private String officialHash;
+    private String calculatedHash;
+
+    public String getAlgorithm() {
+        return algorithm;
+    }
+
+    public String getCalculatedHash() {
+        return calculatedHash;
+    }
+
+    public Path getInputFile() {
+        if (!(input instanceof Path))
+            throw new ClassCastException("Cannot cast input object to path");
+
+        return ((Path) input);
+    }
+
+    public String getInputText() {
+        if (!(input instanceof String))
+            throw new ClassCastException("Cannot cast input object to string");
+
+        return ((String) input);
+    }
+
+    public String getOfficialHash() {
+        return officialHash;
+    }
+
+    public void setAlgorithm(String algorithm) {
+        this.algorithm = algorithm;
+    }
+
+    public void setCalculatedHash(String calculatedHash) {
+        this.calculatedHash = calculatedHash;
+    }
+
+    public void setInputFile(Path file) {
+        this.input = file;
+    }
+
+    public void setInputText(String text) {
+        this.input = text;
+    }
+
+    public void setOfficialHash(String officialHash) {
+        this.officialHash = officialHash;
+    }
 }
