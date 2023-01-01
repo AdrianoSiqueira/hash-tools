6530fb06bf045e7864729fa0d89724964780e5b8
adriano <as.adriano.siqueira@gmail.com>
2023-01-01 14:01:45 -0300

    feat: Add algorithm service.

---
+++ src/main/java/hashtools/core/service/AlgorithmService.java
@@ -0,0 +1,18 @@
+package hashtools.core.service;
+
+public class AlgorithmService {
+
+    public boolean algorithmIsValid(String algorithm) {
+        switch (algorithm) {
+            case "MD5":
+            case "SHA-512":
+            case "SHA-384":
+            case "SHA-256":
+            case "SHA-224":
+            case "SHA-1":
+                return true;
+            default:
+                return false;
+        }
+    }
+}
