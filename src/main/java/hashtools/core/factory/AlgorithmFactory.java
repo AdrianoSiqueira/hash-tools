5470e7252d5ef75034d24401e65c8ab4ea782a09
adriano <as.adriano.siqueira@gmail.com>
2022-12-31 21:15:41 -0300

    feat: Add method to get a algorithm from its name.

--- src/main/java/hashtools/core/factory/AlgorithmFactory.java
+++ src/main/java/hashtools/core/factory/AlgorithmFactory.java
@@ -20,4 +20,23 @@ public class AlgorithmFactory {
                 throw new IllegalArgumentException("The given size does not fit any algorithm: '" + size + "'");
         }
     }
+
+    public String getAlgorithm(String name) {
+        switch (name.replaceAll("[^a-zA-Z0-9]", "")) {
+            case "md5":
+                return "MD5";
+            case "sha1":
+                return "SHA-1";
+            case "sha224":
+                return "SHA-224";
+            case "sha256":
+                return "SHA-256";
+            case "sha384":
+                return "SHA-384";
+            case "sha512":
+                return "SHA-512";
+            default:
+                throw new IllegalArgumentException("The given name does not fit any algorithm: '" + name + "'");
+        }
+    }
 }
