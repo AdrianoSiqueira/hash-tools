a4c6006b13b353f4544ef0947bd4431c1066449f
adriano <as.adriano.siqueira@gmail.com>
2023-01-09 22:01:56 -0300

    refactor: Use 'AlgorithmFactory' to determine if an algorithm is valid.

--- src/main/java/hashtools/core/service/AlgorithmService.java
+++ src/main/java/hashtools/core/service/AlgorithmService.java
@@ -1,21 +1,10 @@
 package hashtools.core.service;
 
+import hashtools.core.factory.AlgorithmFactory;
+
 public class AlgorithmService {
 
     public boolean algorithmIsValid(String algorithm) {
-        if (algorithm == null)
-            return false;
-
-        switch (algorithm) {
-            case "MD5":
-            case "SHA-512":
-            case "SHA-384":
-            case "SHA-256":
-            case "SHA-224":
-            case "SHA-1":
-                return true;
-            default:
-                return false;
-        }
+        return new AlgorithmFactory().getAlgorithm(algorithm) != null;
     }
 }
