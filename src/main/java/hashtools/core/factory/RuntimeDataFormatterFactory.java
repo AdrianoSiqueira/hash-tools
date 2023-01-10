f268def93d6f327edac3e628b0a21d7322b3e6ef
adriano <as.adriano.siqueira@gmail.com>
2023-01-09 21:56:05 -0300

    refactor: Return 'RuntimeDataFormatter' instead of 'Formatter'.

--- src/main/java/hashtools/core/factory/RuntimeDataFormatterFactory.java
+++ src/main/java/hashtools/core/factory/RuntimeDataFormatterFactory.java
@@ -1,13 +1,12 @@
 package hashtools.core.factory;
 
-import hashtools.core.formatter.Formatter;
 import hashtools.core.formatter.RuntimeDataCheckFormatter;
+import hashtools.core.formatter.RuntimeDataFormatter;
 import hashtools.core.formatter.RuntimeDataGenerateFormatter;
-import hashtools.core.model.RuntimeData;
 
 public class RuntimeDataFormatterFactory {
 
-    public Formatter<RuntimeData> getFormatter(boolean checking) {
+    public RuntimeDataFormatter getFormatter(boolean checking) {
         return checking
                ? new RuntimeDataCheckFormatter()
                : new RuntimeDataGenerateFormatter();
