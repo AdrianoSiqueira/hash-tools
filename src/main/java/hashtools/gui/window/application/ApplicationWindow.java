e7af2d880f51f681411b1b30359021c3a57e7b01
adriano <as.adriano.siqueira@gmail.com>
2023-01-13 18:54:26 -0300

    refactor: Call controller directly.

--- src/main/java/hashtools/gui/window/application/ApplicationWindow.java
+++ src/main/java/hashtools/gui/window/application/ApplicationWindow.java
@@ -1,14 +1,7 @@
 package hashtools.gui.window.application;
 
-import hashtools.core.language.LanguageManager;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 /**
  * <p>
@@ -22,37 +15,7 @@ import java.io.IOException;
 public class ApplicationWindow extends Application {
 
     @Override
-    public void start(Stage stage) throws IOException {
-        stage.getProperties().put("host.services", getHostServices());
-        stage.setTitle("HashTools");
-        stage.setScene(createScene());
-        stage.getIcons().add(loadAppIcon());
-        stage.show();
-    }
-
-    /**
-     * <p>
-     * Creates the scene for this window.
-     * </p>
-     *
-     * @return A scene with the content from the fxml file.
-     *
-     * @since 1.0.0
-     */
-    private Scene createScene() throws IOException {
-        FXMLLoader loader = new FXMLLoader(
-                getClass().getResource("Application.fxml"),
-                LanguageManager.getBundle()
-        );
-
-        Parent root = loader.load();
-        root.getStylesheets().add(getClass().getResource("Application.css").toString());
-
-        return new Scene(root);
-    }
-
-    @SuppressWarnings("ConstantConditions")
-    private Image loadAppIcon() {
-        return new Image(getClass().getResourceAsStream("/hashtools/gui/image/application-icon.png"));
+    public void start(Stage stage) {
+        new ApplicationController().start(stage);
     }
 }
