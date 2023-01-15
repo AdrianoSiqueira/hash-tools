fb9dcd372e2bb9d3612761c4f6e3dfb0ef71e59b
adriano <as.adriano.siqueira@gmail.com>
2023-01-15 16:48:32 -0300

    feat: Add 'AbstractController'.

---
+++ src/main/java/hashtools/gui/window/AbstractController.java
@@ -0,0 +1,61 @@
+package hashtools.gui.window;
+
+import hashtools.core.language.LanguageManager;
+import hashtools.core.service.WebService;
+import javafx.application.HostServices;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Optional;
+
+public abstract class AbstractController {
+
+    protected String fxmlPath;
+    protected String stylesheetPath;
+
+    protected WebService webService;
+    protected Logger     logger;
+
+    private void addStylesheetIfPresent(Parent root) {
+        Optional.ofNullable(stylesheetPath)
+                .map(getClass()::getResource)
+                .map(URL::toString)
+                .ifPresent(root.getStylesheets()::add);
+    }
+
+    public abstract void launch(Stage stage);
+
+    protected void loadFxml(AbstractController abstractController) {
+        if (fxmlPath == null) {
+            throw new IllegalStateException("FXML path was not provided.");
+        }
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource(fxmlPath));
+        loader.setController(abstractController);
+        loader.setResources(LanguageManager.getBundle());
+
+        try {
+            Parent root = loader.load();
+            addStylesheetIfPresent(root);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void loadLogger(AbstractController abstractController) {
+        logger = LoggerFactory.getLogger(abstractController.getClass());
+    }
+
+    protected void loadWebService(Stage stage) {
+        Optional.ofNullable(stage.getProperties().get("host.services"))
+                .map(HostServices.class::cast)
+                .map(WebService::new)
+                .ifPresent(webService -> this.webService = webService);
+    }
+}
