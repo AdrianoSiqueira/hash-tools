f582bea99b121f7727d022f2762f3d6a59913e8c
adriano <as.adriano.siqueira@gmail.com>
2023-01-22 20:57:17 -0300

    feat: Add 'DataOperation' interface.

---
+++ src/main/java/hashtools/core/operation/data/DataOperation.java
@@ -0,0 +1,7 @@
+package hashtools.core.operation.data;
+
+import hashtools.core.model.Data;
+import hashtools.core.operation.Operation;
+
+public interface DataOperation extends Operation<Data> {
+}
