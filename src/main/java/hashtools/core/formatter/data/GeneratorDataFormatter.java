2ab658437094acbe8208dc6c797f22fe1e064000
adriano <as.adriano.siqueira@gmail.com>
2023-01-22 20:55:20 -0300

    feat: Add 'GeneratorDataFormatter' class.

---
+++ src/main/java/hashtools/core/formatter/data/GeneratorDataFormatter.java
@@ -0,0 +1,25 @@
+package hashtools.core.formatter.data;
+
+import hashtools.core.model.Data;
+
+import java.util.StringJoiner;
+
+public class GeneratorDataFormatter extends DataFormatter {
+
+    @Override
+    public String format(Data data) {
+        String name = data.isUsingInputFile()
+                      ? data.getInputFile().getFileName().toString()
+                      : data.getInputText();
+
+        StringJoiner result = new StringJoiner("\n");
+
+        sortHashes(data.getHashes()).forEach(hash -> {
+            String content = hash.getGenerated() + "  \"" + name + '\"';
+
+            result.add(content);
+        });
+
+        return result.toString();
+    }
+}
