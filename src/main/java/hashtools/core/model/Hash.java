3c0b2cadce3f9931c8cd3ff837d8d6ce3a2be41f
adriano <as.adriano.siqueira@gmail.com>
2023-01-22 20:48:10 -0300

    refactor!: Add 'algorithm' attribute.

--- src/main/java/hashtools/core/model/Hash.java
+++ src/main/java/hashtools/core/model/Hash.java
@@ -1,11 +1,29 @@
 package hashtools.core.model;
 
+import java.util.Objects;
+
 public class Hash {
 
+    private String algorithm;
     private String official;
     private String generated;
 
     public Hash() {
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null) return false;
+        if (getClass() != object.getClass()) return false;
+
+        Hash hash = (Hash) object;
+
+        return Objects.equals(algorithm, hash.algorithm);
+    }
+
+    public String getAlgorithm() {
+        return algorithm;
     }
 
     public String getGenerated() {
@@ -16,11 +34,20 @@ public class Hash {
         return official;
     }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(algorithm);
+    }
+
     public boolean matches() {
         if (official == null) return false;
         if (generated == null) return false;
 
         return official.equalsIgnoreCase(generated);
+    }
+
+    public void setAlgorithm(String algorithm) {
+        this.algorithm = algorithm;
     }
 
     public void setGenerated(String generated) {
