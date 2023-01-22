4191ac5c13c8402e4b0b6f40280c038bc7ba1d7f
adriano <as.adriano.siqueira@gmail.com>
2023-01-22 20:51:51 -0300

    feat: Add 'HashFactory' interface.

---
+++ src/main/java/hashtools/core/factory/hash/HashFactory.java
@@ -0,0 +1,9 @@
+package hashtools.core.factory.hash;
+
+import hashtools.core.factory.Factory;
+import hashtools.core.model.Hash;
+
+import java.util.List;
+
+public interface HashFactory extends Factory<List<Hash>> {
+}
