44d14306a56db5916b0636df1231e0affd695f3b
adriano <as.adriano.siqueira@gmail.com>
2023-01-25 22:01:11 -0300

    feat: Add 'TextInputControl' object as destination for data.

--- src/main/java/hashtools/core/consumer/data/GUIDataConsumer.java
+++ src/main/java/hashtools/core/consumer/data/GUIDataConsumer.java
@@ -2,13 +2,21 @@ package hashtools.core.consumer.data;
 
 import hashtools.core.model.Data;
 import javafx.scene.control.Labeled;
+import javafx.scene.control.TextInputControl;
 
 public class GUIDataConsumer implements DataConsumer {
 
-    private final Labeled node;
+    private final Labeled          labeled;
+    private final TextInputControl textInputControl;
 
-    public GUIDataConsumer(Labeled node) {
-        this.node = node;
+    public GUIDataConsumer(Labeled labeled) {
+        this.labeled          = labeled;
+        this.textInputControl = null;
+    }
+
+    public GUIDataConsumer(TextInputControl textInputControl) {
+        this.labeled          = null;
+        this.textInputControl = textInputControl;
     }
 
     @Override
@@ -16,6 +24,10 @@ public class GUIDataConsumer implements DataConsumer {
         String result = data.getFormatter()
                             .format(data);
 
-        node.setText(result);
+        if (labeled != null)
+            labeled.setText(result);
+
+        if (textInputControl != null)
+            textInputControl.setText(result);
     }
 }
