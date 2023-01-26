e68130da7aa3dd3b923b99a09131499d19d97894
adriano <as.adriano.siqueira@gmail.com>
2023-01-26 13:52:51 -0300

    feat: Add 'ProgressBar' component.

--- src/main/java/hashtools/core/consumer/data/GUIDataConsumer.java
+++ src/main/java/hashtools/core/consumer/data/GUIDataConsumer.java
@@ -2,21 +2,31 @@ package hashtools.core.consumer.data;
 
 import hashtools.core.model.Data;
 import javafx.scene.control.Labeled;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextInputControl;
 
 public class GUIDataConsumer implements DataConsumer {
 
     private final Labeled          labeled;
     private final TextInputControl textInputControl;
+    private final ProgressBar      progressBar;
 
     public GUIDataConsumer(Labeled labeled) {
         this.labeled          = labeled;
         this.textInputControl = null;
+        this.progressBar      = null;
     }
 
     public GUIDataConsumer(TextInputControl textInputControl) {
         this.labeled          = null;
         this.textInputControl = textInputControl;
+        this.progressBar      = null;
+    }
+
+    public GUIDataConsumer(ProgressBar progressBar) {
+        this.labeled          = null;
+        this.textInputControl = null;
+        this.progressBar      = progressBar;
     }
 
     @Override
@@ -29,5 +39,8 @@ public class GUIDataConsumer implements DataConsumer {
 
         if (textInputControl != null)
             textInputControl.setText(result);
+
+        if (progressBar != null)
+            progressBar.setProgress(data.getSafetyPercentage() / 100.0);
     }
 }
