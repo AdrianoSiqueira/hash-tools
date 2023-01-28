34dbb8093108877b597661c00278a6983eb98521
adriano <as.adriano.siqueira@gmail.com>
2023-01-28 20:08:35 -0300

    refactor: Implement parallelism in hash generation.

--- src/main/java/hashtools/core/operation/data/HashGeneratorDataOperation.java
+++ src/main/java/hashtools/core/operation/data/HashGeneratorDataOperation.java
@@ -1,22 +1,49 @@
 package hashtools.core.operation.data;
 
 import hashtools.core.model.Data;
+import hashtools.core.model.Hash;
 import hashtools.core.service.HashService;
+import hashtools.core.service.ParallelismService;
 
-/**
- * TODO Implement parallelism service in this class.
- */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+
 public class HashGeneratorDataOperation implements DataOperation {
+
+    private final HashService service;
+
+    public HashGeneratorDataOperation() {
+        service = new HashService();
+    }
+
+    private void generateHash(Data data, Hash hash) {
+        hash.setGenerated(
+                data.isUsingInputFile()
+                ? service.generate(hash.getAlgorithm(), data.getInputFile())
+                : service.generate(hash.getAlgorithm(), data.getInputText())
+        );
+    }
 
     @Override
     public void perform(Data data) {
-        HashService service = new HashService();
+        List<Hash> hashes = data.getHashes();
 
-        data.getHashes()
-            .forEach(hash -> hash.setGenerated(
-                    data.isUsingInputFile()
-                    ? service.generate(hash.getAlgorithm(), data.getInputFile())
-                    : service.generate(hash.getAlgorithm(), data.getInputText())
-            ));
+        ExecutorService            executor = ParallelismService.INSTANCE.getFixedThreadPool();
+        List<CompletableFuture<?>> futures  = new ArrayList<>();
+
+        hashes.forEach(hash -> futures.add(CompletableFuture.runAsync(
+                () -> generateHash(data, hash),
+                executor
+        )));
+
+        try {
+            CompletableFuture.allOf(futures.toArray(CompletableFuture[]::new))
+                             .get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 }
