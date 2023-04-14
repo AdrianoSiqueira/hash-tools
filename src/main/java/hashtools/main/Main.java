a00684bd2b7719de796eee0f055b13c0a5de4cf8
adriano <as.adriano.siqueira@gmail.com>
2023-04-13 21:40:01 -0300

    Remove the shutdown of 'ParallelismService' class.

--- src/main/java/hashtools/main/Main.java
+++ src/main/java/hashtools/main/Main.java
@@ -3,22 +3,14 @@ package hashtools.main;
 import hashtools.core.runner.CLIRunner;
 import hashtools.core.runner.GUIRunner;
 import hashtools.core.runner.Runner;
-import hashtools.core.service.ParallelismService;
 
 public class Main {
 
     public static void main(String[] args) {
-        scheduleParallelismServiceShutdown();
-
         Runner runner = (args.length == 0)
                         ? new GUIRunner()
                         : new CLIRunner(args);
 
         runner.run();
-    }
-
-    private static void scheduleParallelismServiceShutdown() {
-        Runtime.getRuntime()
-               .addShutdownHook(new Thread(ParallelismService::shutdown));
     }
 }
