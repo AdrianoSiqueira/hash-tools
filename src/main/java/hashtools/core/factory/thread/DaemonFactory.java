8e2a30d243538fdab79d9aedcc915689ce1b3840
adriano <as.adriano.siqueira@gmail.com>
2023-04-13 21:32:25 -0300

    Mark class for removal.

--- src/main/java/hashtools/core/factory/thread/DaemonFactory.java
+++ src/main/java/hashtools/core/factory/thread/DaemonFactory.java
@@ -2,6 +2,7 @@ package hashtools.core.factory.thread;
 
 import java.util.concurrent.ThreadFactory;
 
+@Deprecated(forRemoval = true)
 public class DaemonFactory implements ThreadFactory {
 
     @Override
