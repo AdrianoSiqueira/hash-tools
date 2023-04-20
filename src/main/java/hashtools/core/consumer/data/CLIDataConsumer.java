ca6a591db40b69497c85d5560ee1ebd34fe147ec
adriano <as.adriano.siqueira@gmail.com>
2023-04-19 22:13:35 -0300

    Add javadoc to 'CLIDataConsumer'.

--- src/main/java/hashtools/core/consumer/data/CLIDataConsumer.java
+++ src/main/java/hashtools/core/consumer/data/CLIDataConsumer.java
@@ -2,6 +2,12 @@ package hashtools.core.consumer.data;
 
 import hashtools.core.model.Data;
 
+/**
+ * <p>
+ * Consumes the {@link Data} object printing its formatted content to the
+ * console.
+ * </p>
+ */
 public class CLIDataConsumer implements DataConsumer {
 
     @Override
