e95af2626772f6eae7ad421883b9e370e72109ab
adriano <as.adriano.siqueira@gmail.com>
2023-05-02 20:47:10 -0300

    Add javadoc to 'FileService'.

--- src/main/java/hashtools/core/service/FileService.java
+++ src/main/java/hashtools/core/service/FileService.java
@@ -8,8 +8,22 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/**
+ * <p>
+ * Performs operations involving files.
+ * </p>
+ */
 public class FileService {
 
+    /**
+     * <p>
+     * Reads all lines of the file.
+     * </p>
+     *
+     * @param file File to be read.
+     *
+     * @return A list with the lines.
+     */
     public List<String> readLines(Path file) {
         if (file == null)
             return List.of();
@@ -22,6 +36,15 @@ public class FileService {
         }
     }
 
+    /**
+     * <p>
+     * Reads the official file separating the data from its lines.
+     * </p>
+     *
+     * @param file File to be read.
+     *
+     * @return A list with the official hashes.
+     */
     public List<String> readOfficialFile(Path file) {
         return readLines(file)
                 .stream()
@@ -30,6 +53,14 @@ public class FileService {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * <p>
+     * Replaces the content of the file.
+     * </p>
+     *
+     * @param content New content to be saved.
+     * @param file    Where to save the content.
+     */
     public void replaceContent(String content, Path file) {
         if (content == null) return;
         if (file == null) return;
