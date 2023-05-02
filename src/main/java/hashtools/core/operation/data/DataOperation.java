c300a049a129451384682bd59f94de0b731cf26b
adriano <as.adriano.siqueira@gmail.com>
2023-05-02 20:37:35 -0300

    Add javadoc to 'DataOperation'.

--- src/main/java/hashtools/core/operation/data/DataOperation.java
+++ src/main/java/hashtools/core/operation/data/DataOperation.java
@@ -3,5 +3,11 @@ package hashtools.core.operation.data;
 import hashtools.core.model.Data;
 import hashtools.core.operation.Operation;
 
+/**
+ * <p>
+ * Specialization of {@link Operation} destined to work with
+ * {@link Data} objects.
+ * </p>
+ */
 public interface DataOperation extends Operation<Data> {
 }
