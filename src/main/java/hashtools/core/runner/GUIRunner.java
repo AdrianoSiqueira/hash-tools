0a189e21bcf1d2c43f0b34d4b020aa11edfb10dd
adriano <as.adriano.siqueira@gmail.com>
2023-05-02 20:43:15 -0300

    Add javadoc to 'GUIRunner'.

--- src/main/java/hashtools/core/runner/GUIRunner.java
+++ src/main/java/hashtools/core/runner/GUIRunner.java
@@ -4,6 +4,12 @@ import hashtools.gui.window.application.ApplicationWindow;
 import hashtools.gui.window.preloader.PreloaderWindow;
 import javafx.application.Application;
 
+/**
+ * <p>
+ * Sets the preloader window and launches the graphical user
+ * interface.
+ * </p>
+ */
 public class GUIRunner implements Runner {
 
     private void initPreloader() {
