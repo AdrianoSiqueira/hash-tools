534f7e79000944eb2331dcde7eabdb65d048a455
adriano <as.adriano.siqueira@gmail.com>
2023-05-02 20:37:28 -0300

    Add javadoc to 'Operation'.

--- src/main/java/hashtools/core/operation/Operation.java
+++ src/main/java/hashtools/core/operation/Operation.java
@@ -1,5 +1,13 @@
 package hashtools.core.operation;
 
+/**
+ * <p>
+ * Simple interface to perform some operation in an object of
+ * type {@link T}.
+ * </p>
+ *
+ * @param <T> Type of object the operation will be performed in.
+ */
 public interface Operation<T> {
 
     void perform(T t);
