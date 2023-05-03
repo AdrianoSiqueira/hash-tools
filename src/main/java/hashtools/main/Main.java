c1a7525aac5a770bdc725c10f0b56582abac8c25
adriano <as.adriano.siqueira@gmail.com>
2023-05-02 21:06:52 -0300

    Add javadoc to 'Main'.

--- src/main/java/hashtools/main/Main.java
+++ src/main/java/hashtools/main/Main.java
@@ -4,6 +4,12 @@ import hashtools.core.runner.CLIRunner;
 import hashtools.core.runner.GUIRunner;
 import hashtools.core.runner.Runner;
 
+/**
+ * <p>
+ * Application main class. It determines the running mode based on
+ * the content of the arguments from the command line.
+ * </p>
+ */
 public class Main {
 
     public static void main(String[] args) {
