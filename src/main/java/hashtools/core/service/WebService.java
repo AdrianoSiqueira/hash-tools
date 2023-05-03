3dfa397469918b99c7295929b29fb3da856913f8
adriano <as.adriano.siqueira@gmail.com>
2023-05-02 21:02:46 -0300

    Add javadoc to 'WebService'.

--- src/main/java/hashtools/core/service/WebService.java
+++ src/main/java/hashtools/core/service/WebService.java
@@ -2,6 +2,13 @@ package hashtools.core.service;
 
 import javafx.application.HostServices;
 
+import java.net.URL;
+
+/**
+ * <p>
+ * Performs network operations.
+ * </p>
+ */
 public class WebService {
 
     private HostServices service;
@@ -14,6 +21,14 @@ public class WebService {
         return service;
     }
 
+    /**
+     * <p>
+     * Opens the given {@link URL} with the operating system's
+     * default web browser.
+     * </p>
+     *
+     * @param url URL to open.
+     */
     public void openWebPage(String url) {
         service.showDocument(url);
     }
