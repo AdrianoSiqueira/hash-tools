b80a1a7e2dce157ecfe8f44344ad239d135bd411
adriano <as.adriano.siqueira@gmail.com>
2023-12-25 20:45:04 -0300

    Add 'Algorithm' class.

---
+++ src/main/java/hashtools/domain/Algorithm.java
@@ -0,0 +1,19 @@
+package hashtools.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum Algorithm {
+
+    MD5("MD5", 32),
+    SHA1("SHA-1", 40),
+    SHA224("SHA-224", 56),
+    SHA256("SHA-256", 64),
+    SHA384("SHA-384", 96),
+    SHA512("SHA-512", 128);
+
+    private final String name;
+    private final int    length;
+}
