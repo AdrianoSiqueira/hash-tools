3dfee506eb634ed7d85dd8ef4d1d7f31f183d7c1
adriano <as.adriano.siqueira@gmail.com>
2023-12-25 20:07:27 -0300

    Add MessageDigest updater file implementation.

---
+++ src/main/java/hashtools/domain/messagedigest/FileUpdater.java
@@ -0,0 +1,31 @@
+package hashtools.domain.messagedigest;
+
+import lombok.AllArgsConstructor;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.MessageDigest;
+
+@AllArgsConstructor
+public class FileUpdater implements Updater {
+
+    private static final int BUFFER_SIZE = 2048;
+
+    private Path file;
+
+    @Override
+    public void update(MessageDigest messageDigest) {
+        try (InputStream stream = Files.newInputStream(file)) {
+            int    read;
+            byte[] buffer = new byte[BUFFER_SIZE];
+
+            while ((read = stream.read(buffer)) != -1) {
+                messageDigest.update(buffer, 0, read);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
