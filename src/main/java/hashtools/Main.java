849c972d85bbac1d6de11789f655555c65a439f9
adriano <as.adriano.siqueira@gmail.com>
2024-01-01 14:47:55 -0300

    Implement gui launching.

--- src/main/java/hashtools/Main.java
+++ src/main/java/hashtools/Main.java
@@ -1,8 +1,12 @@
 package hashtools;
 
+import hashtools.controller.Preloader;
+import javafx.application.Application;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        System.setProperty("javafx.preloader", Preloader.class.getCanonicalName());
+        Application.launch(hashtools.controller.Application.class);
     }
 }
