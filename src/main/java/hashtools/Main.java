ea3200d4829192f771644718c032a9f19d792ed1
adriano <as.adriano.siqueira@gmail.com>
2024-01-01 20:01:03 -0300

    Init language manager.

--- src/main/java/hashtools/Main.java
+++ src/main/java/hashtools/Main.java
@@ -1,11 +1,17 @@
 package hashtools;
 
 import hashtools.controller.Preloader;
+import hashtools.language.Language;
 import javafx.application.Application;
+
+import java.util.Locale;
 
 public class Main {
 
     public static void main(String[] args) {
+//        Locale.setDefault(new Locale("pt", "br"));
+        Language.init();
+
         System.setProperty("javafx.preloader", Preloader.class.getCanonicalName());
         Application.launch(hashtools.controller.Application.class);
     }
