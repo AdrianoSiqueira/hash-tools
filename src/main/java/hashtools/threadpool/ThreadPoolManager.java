134c1f3baaabc6b2777b3864ae9be0406b435093
adriano <as.adriano.siqueira@gmail.com>
2024-01-08 20:26:05 -0300

    Add 'getCallerName' method.

--- src/main/java/hashtools/threadpool/ThreadPoolManager.java
+++ src/main/java/hashtools/threadpool/ThreadPoolManager.java
@@ -2,6 +2,7 @@ package hashtools.threadpool;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.function.Predicate;
 
 public class ThreadPoolManager {
 
@@ -10,6 +11,28 @@ public class ThreadPoolManager {
             Runtime.getRuntime().availableProcessors(),
             new ThreadPoolFactory(name, isDaemon)
         );
+    }
+
+    private static String getCallerName() {
+        /*
+         * This predicate removes this class from the caller
+         * stream, allowing to get the imediate caller class
+         * even if this method is called internally multiple
+         * times.
+         */
+        Predicate<Class<?>> removeThisClass = clazz -> !clazz
+            .getName()
+            .equals(ThreadPoolManager.class.getName());
+
+        return StackWalker
+            .getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE)
+            .walk(stream -> stream
+                .map(StackWalker.StackFrame::getDeclaringClass)
+                .filter(removeThisClass)
+                .limit(1)
+                .findFirst()
+                .map(Class::getSimpleName)
+                .orElse(null));
     }
 
     public static ExecutorService newDaemon(String name) {
