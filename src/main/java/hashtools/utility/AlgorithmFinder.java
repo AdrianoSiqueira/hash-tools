c6f1dfbb1eb8ac4cce27843f74f813ce3528a57b
adriano <as.adriano.siqueira@gmail.com>
2024-01-15 21:25:45 -0300

    Inline predicates.

--- src/main/java/hashtools/utility/AlgorithmFinder.java
+++ src/main/java/hashtools/utility/AlgorithmFinder.java
@@ -10,16 +10,17 @@ public class AlgorithmFinder {
     public Optional<Algorithm> find(int length) {
         return Stream
             .of(Algorithm.values())
-            .filter(lengthMatches)
-            .findFirst()
-            .orElse(null);
+            .filter(algorithm -> algorithm.getLength() == length)
+            .findFirst();
     }
 
     public Optional<Algorithm> find(String name) {
         return Stream
             .of(Algorithm.values())
-            .filter(nameMatches)
-            .findFirst()
-            .orElse(null);
+            .filter(algorithm -> algorithm
+                .getName()
+                .replace("-", "")
+                .equalsIgnoreCase(name))
+            .findFirst();
     }
 }
