48c230cc2b4cb01ad3a448e6ddc313a976f47839
adriano <as.adriano.siqueira@gmail.com>
2024-01-30 21:54:51 -0300

    Add evaluate method.

--- src/main/java/hashtools/controller/Controller.java
+++ src/main/java/hashtools/controller/Controller.java
@@ -1,5 +1,7 @@
 package hashtools.controller;
 
+import hashtools.domain.exception.FileNotFoundException;
+import hashtools.domain.exception.NullParameterException;
 import hashtools.language.Language;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,6 +12,8 @@ import java.io.IOException;
 public interface Controller {
 
     default Scene createScene(String fxmlPath) {
+        evaluate(fxmlPath);
+
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource(fxmlPath));
         loader.setResources(Language.INSTANCE.getBundle());
@@ -19,6 +23,16 @@ public interface Controller {
             return new Scene(pane);
         } catch (IOException e) {
             throw new RuntimeException("Could not load fxml", e);
+        }
+    }
+
+    default void evaluate(String path) {
+        if (path == null) {
+            throw new NullParameterException("'fxmlPath'");
+        }
+
+        if (getClass().getResource(path) == null) {
+            throw new FileNotFoundException("'%s'".formatted(path));
         }
     }
 }
