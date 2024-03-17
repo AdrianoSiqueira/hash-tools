ccb395b21ce1523a991cafddbb1a82532f562c2c
adriano <as.adriano.siqueira@gmail.com>
2024-03-16 22:34:29 -0300

    Add language resource bundle.

--- src/main/java/hashtools/domain/Environment.java
+++ src/main/java/hashtools/domain/Environment.java
@@ -2,6 +2,8 @@ package hashtools.domain;
 
 import hashtools.threadpool.DaemonThreadFactory;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -13,6 +15,7 @@ public final class Environment {
 
     public static final class Software {
         public static final ExecutorService THREAD_POOL = Executors.newCachedThreadPool(new DaemonThreadFactory("GlobalPool"));
+        public static final ResourceBundle LANGUAGE = ResourceBundle.getBundle("hashtools.language.language", Locale.getDefault());
         public static final String VERSION = "3.0.0";
     }
 }
