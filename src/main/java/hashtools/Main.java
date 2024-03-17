5a460624535e3d6f37af29b0502692a529831020
adriano <as.adriano.siqueira@gmail.com>
2024-03-16 22:47:32 -0300

    Remove language initialization.

--- src/main/java/hashtools/Main.java
+++ src/main/java/hashtools/Main.java
@@ -2,14 +2,12 @@ package hashtools;
 
 import hashtools.controller.ApplicationController;
 import hashtools.controller.PreloaderController;
-import hashtools.language.Language;
 import javafx.application.Application;
 
 public class Main {
 
     public static void main(String[] args) {
 //        Locale.setDefault(new Locale("pt", "br"));
-        Language.init();
 
         System.setProperty("javafx.preloader", PreloaderController.class.getCanonicalName());
         Application.launch(ApplicationController.class);
