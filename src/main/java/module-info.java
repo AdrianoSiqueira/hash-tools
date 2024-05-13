e3658bc8580370c223d7f68efce61cadb420f549
adriano <as.adriano.siqueira@gmail.com>
2024-05-12 22:16:37 -0300

    Add exports to 'dialog' package.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,11 @@ module hash.tools {
     requires javafx.fxml;
     requires javafx.controls;
     requires org.slf4j;
+    requires java.sql;
 
     exports hashtools.controller;
+    exports hashtools.dialog;
+
     opens hashtools.controller;
+    opens hashtools.dialog;
 }
