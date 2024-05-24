a0c2f066fc1960cb49af42a96bc9b67de0936a9c
adriano <as.adriano.siqueira@gmail.com>
2024-05-23 22:19:30 -0300

    Remove JavaFX thread statement.

--- src/main/java/hashtools/controller/application/ApplicationModule.java
+++ src/main/java/hashtools/controller/application/ApplicationModule.java
@@ -1,18 +1,11 @@
 package hashtools.controller.application;
 
 import hashtools.domain.Condition;
-import javafx.application.Platform;
-
-import java.util.concurrent.atomic.AtomicBoolean;
 
 interface ApplicationModule {
 
     default boolean isNotReadyToRun(Condition<ConditionData> condition, ConditionData data) {
-        var hasProblem = new AtomicBoolean();
-
-        Platform.runLater(() -> hasProblem.set(condition.isFalse(data)));
-
-        return hasProblem.get();
+        return condition.isFalse(data);
     }
 
     void run();
