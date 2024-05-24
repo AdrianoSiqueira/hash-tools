f91ecf252f9930107554f3ad6a608e1820d599ef
adriano <as.adriano.siqueira@gmail.com>
2024-05-23 22:23:44 -0300

    Replace MessageDialog with exception.

--- src/main/java/hashtools/controller/application/CanRunComparatorCondition.java
+++ src/main/java/hashtools/controller/application/CanRunComparatorCondition.java
@@ -1,8 +1,7 @@
 package hashtools.controller.application;
 
-import hashtools.dialog.MessageDialog;
 import hashtools.domain.Condition;
-import javafx.application.Platform;
+import hashtools.domain.exception.ConditionNotMetException;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,30 +10,12 @@ class CanRunComparatorCondition implements Condition<ConditionData> {
 
     @Override
     public boolean isTrue(ConditionData data) {
-        if (!Platform.isFxApplicationThread()) {
-            throw new IllegalStateException("This class must be called from a JavaFX thread");
-        }
-
         if (data.isCheckUseFile1Selected() && !Files.isRegularFile(Path.of(data.getField1Content()))) {
-            MessageDialog
-                .builder()
-                .title("Application Controller")
-                .content("The first file is not valid.")
-                .build()
-                .configure()
-                .showAndWait();
-            return false;
+            throw new ConditionNotMetException("The first file is not valid.");
         }
 
         if (data.isCheckUseFile2Selected() && !Files.isRegularFile(Path.of(data.getField2Content()))) {
-            MessageDialog
-                .builder()
-                .title("Application Controller")
-                .content("The second file is not valid.")
-                .build()
-                .configure()
-                .showAndWait();
-            return false;
+            throw new ConditionNotMetException("The second file is not valid.");
         }
 
         return true;
--- src/main/java/hashtools/controller/application/CanRunGeneratorCondition.java
+++ src/main/java/hashtools/controller/application/CanRunGeneratorCondition.java
@@ -1,8 +1,7 @@
 package hashtools.controller.application;
 
-import hashtools.dialog.MessageDialog;
 import hashtools.domain.Condition;
-import javafx.application.Platform;
+import hashtools.domain.exception.ConditionNotMetException;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,37 +10,19 @@ class CanRunGeneratorCondition implements Condition<ConditionData> {
 
     @Override
     public boolean isTrue(ConditionData data) {
-        if (!Platform.isFxApplicationThread()) {
-            throw new IllegalStateException("This class must be called from a JavaFX thread");
-        }
-
         if (data.isCheckUseFile1Selected() && !Files.isRegularFile(Path.of(data.getField1Content()))) {
-            MessageDialog
-                .builder()
-                .title("Application Controller")
-                .content("The input file is not valid.")
-                .build()
-                .configure()
-                .showAndWait();
-            return false;
+            throw new ConditionNotMetException("The input file is not valid.");
         }
 
-        var noAlgorithmSelected = !data.isCheckMD5Selected()
+        var noAlgorithmIsSelected = !data.isCheckMD5Selected()
             && !data.isCheckSHA1Selected()
             && !data.isCheckSHA224Selected()
             && !data.isCheckSHA256Selected()
             && !data.isCheckSHA384Selected()
             && !data.isCheckSHA512Selected();
 
-        if (noAlgorithmSelected) {
-            MessageDialog
-                .builder()
-                .title("Application Controller")
-                .content("No algorithms selected.")
-                .build()
-                .configure()
-                .showAndWait();
-            return false;
+        if (noAlgorithmIsSelected) {
+            throw new ConditionNotMetException("No algorithms are selected.");
         }
 
         return true;
