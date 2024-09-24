7cd3b5d028af5ab52362f40dedad88b25c3035ff
adriano <as.adriano.siqueira@gmail.com>
2024-09-23 21:38:46 -0300

    Add 'ComparatorResponse' class.

---
+++ src/main/java/hashtools/domain/ComparatorResponse.java
@@ -0,0 +1,15 @@
+package hashtools.domain;
+
+import hashtools.identification.Identification;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class ComparatorResponse {
+    private Identification identification1;
+    private Identification identification2;
+    private ComparatorChecksum checksum;
+}
