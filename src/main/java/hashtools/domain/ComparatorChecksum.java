442f0825de87b33083528d5824bf00b8a464d1cb
adriano <as.adriano.siqueira@gmail.com>
2024-09-23 21:38:27 -0300

    Add 'ComparatorChecksum' class.

---
+++ src/main/java/hashtools/domain/ComparatorChecksum.java
@@ -0,0 +1,19 @@
+package hashtools.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class ComparatorChecksum {
+    private Algorithm algorithm;
+    private String hash1;
+    private String hash2;
+
+    public final boolean matches() {
+        return hash1 != null
+            && hash1.equalsIgnoreCase(hash2);
+    }
+}
