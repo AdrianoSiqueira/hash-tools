857f07c64f8ee18e6aaa4d3901d11676b34ee2df
adriano <as.adriano.siqueira@gmail.com>
2024-09-23 21:41:58 -0300

    Add 'StringIdentification' class.

---
+++ src/main/java/hashtools/identification/StringIdentification.java
@@ -0,0 +1,14 @@
+package hashtools.identification;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class StringIdentification implements Identification {
+
+    private final String string;
+
+    @Override
+    public String identity() {
+        return string;
+    }
+}
