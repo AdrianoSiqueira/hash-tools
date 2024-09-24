60982b22a22854fd09c9a35addf2d8950141de1f
adriano <as.adriano.siqueira@gmail.com>
2024-09-23 21:56:41 -0300

    Add required exports to open the gui.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,4 +3,14 @@ module hash.tools {
     requires javafx.fxml;
     requires lombok;
     requires org.slf4j;
+
+    exports hashtools.condition;
+    exports hashtools.controller;
+    exports hashtools.domain;
+    exports hashtools.window;
+
+    opens hashtools.condition;
+    opens hashtools.controller;
+    opens hashtools.domain;
+    opens hashtools.window;
 }
