5c25ad83f6475514d1448e3663ab9286c1b67d04
adriano <as.adriano.siqueira@gmail.com>
2024-09-23 21:37:47 -0300

    Add 'CheckerChecksum' class.

---
+++ src/main/java/hashtools/domain/CheckerChecksum.java
@@ -0,0 +1,19 @@
+package hashtools.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class CheckerChecksum {
+    private Algorithm algorithm;
+    private String generatedHash;
+    private String officialHash;
+
+    public final boolean matches() {
+        return generatedHash != null
+            && generatedHash.equalsIgnoreCase(officialHash);
+    }
+}
