f5d0f857669e1d27a5ec7024b0b28a6ce09d224b
adriano <as.adriano.siqueira@gmail.com>
2024-09-23 21:39:30 -0300

    Add 'DaemonThreadFactory' class.

---
+++ src/main/java/hashtools/domain/DaemonThreadFactory.java
@@ -0,0 +1,13 @@
+package hashtools.domain;
+
+import java.util.concurrent.ThreadFactory;
+
+public class DaemonThreadFactory implements ThreadFactory {
+
+    @Override
+    public Thread newThread(Runnable runnable) {
+        Thread thread = new Thread(runnable);
+        thread.setDaemon(true);
+        return thread;
+    }
+}
