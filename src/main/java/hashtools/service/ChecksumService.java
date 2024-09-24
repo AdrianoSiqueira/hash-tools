505c660e788f7a4e0f64802bb989687c4cd05f9d
adriano <as.adriano.siqueira@gmail.com>
2024-09-23 21:45:49 -0300

    Add 'ChecksumService' class.

---
+++ src/main/java/hashtools/service/ChecksumService.java
@@ -0,0 +1,37 @@
+package hashtools.service;
+
+import hashtools.domain.Algorithm;
+import hashtools.messagedigest.Updater;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class ChecksumService {
+
+    private String decode(byte[] bytes) {
+        return IntStream
+            .range(0, bytes.length)
+            .mapToObj(i -> bytes[i])
+            .map("%02x"::formatted)
+            .collect(Collectors.joining());
+    }
+
+    public String generate(Algorithm algorithm, Updater updater) {
+        MessageDigest messageDigest = getMessageDigest(algorithm);
+        updater.update(messageDigest);
+
+        byte[] bytes = messageDigest.digest();
+        return decode(bytes);
+    }
+
+    private MessageDigest getMessageDigest(Algorithm algorithm) {
+        try {
+            return MessageDigest.getInstance(algorithm.getName());
+        } catch (NoSuchAlgorithmException e) {
+            // Never reach by design
+            throw new RuntimeException(e);
+        }
+    }
+}
