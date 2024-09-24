b106f07db9f848f184b2c0d7c3ebabf3ead8c257
adriano <as.adriano.siqueira@gmail.com>
2024-09-23 21:44:00 -0300

    Add 'OfficialChecksumGetter' interface.

---
+++ src/main/java/hashtools/officialchecksum/OfficialChecksumGetter.java
@@ -0,0 +1,10 @@
+package hashtools.officialchecksum;
+
+import hashtools.domain.CheckerChecksum;
+
+import java.util.List;
+
+public interface OfficialChecksumGetter {
+
+    List<CheckerChecksum> get();
+}
