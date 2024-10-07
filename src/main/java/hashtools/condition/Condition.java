cf5d69fe8de30912b12549bdeb4bbdfa601b8a15
adriano <as.adriano.siqueira@gmail.com>
2024-10-06 23:13:09 -0300

    Add 'allOf' method.

--- src/main/java/hashtools/condition/Condition.java
+++ src/main/java/hashtools/condition/Condition.java
@@ -1,6 +1,14 @@
 package hashtools.condition;
 
+import java.util.Collection;
+
 public interface Condition {
+
+    static Condition allOf(Collection<Condition> conditions) {
+        return () -> conditions
+            .stream()
+            .allMatch(Condition::isTrue);
+    }
 
     boolean isTrue();
 }
