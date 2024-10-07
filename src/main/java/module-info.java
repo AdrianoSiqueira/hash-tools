83174a5395bc2907444f6af8134bb812f57de97b
adriano <as.adriano.siqueira@gmail.com>
2024-10-07 11:00:32 -0300

    Add exports for 'hashtools.operation'.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,10 +7,12 @@ module hash.tools {
     exports hashtools.condition;
     exports hashtools.controller;
     exports hashtools.domain;
+    exports hashtools.operation;
     exports hashtools.window;
 
     opens hashtools.condition;
     opens hashtools.controller;
     opens hashtools.domain;
+    opens hashtools.operation;
     opens hashtools.window;
 }
