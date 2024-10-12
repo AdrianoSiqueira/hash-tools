b7e51108441583561a3a0db7bd0c778a92c9668a
adriano <as.adriano.siqueira@gmail.com>
2024-10-11 22:11:45 -0300

    Remove running condition.

--- src/main/java/hashtools/util/FileUtil.java
+++ src/main/java/hashtools/util/FileUtil.java
@@ -1,9 +1,7 @@
 package hashtools.util;
 
-import hashtools.condition.FileExists;
 import hashtools.operation.OperationPerformer;
 import hashtools.operation.ReplaceFileContent;
-import hashtools.operation.ThrowException;
 
 import java.nio.file.Path;
 
@@ -11,9 +9,7 @@ public class FileUtil {
 
     public static void replaceContent(CharSequence content, Path file) {
         OperationPerformer.perform(
-            new FileExists(file),
-            new ReplaceFileContent(content, file),
-            new ThrowException(new IllegalArgumentException("File does not exists"))
+            new ReplaceFileContent(content, file)
         );
     }
 }
