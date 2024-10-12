2dc0189e755b33093f18ec0c79e730b798efac44
adriano <as.adriano.siqueira@gmail.com>
2024-10-11 21:20:46 -0300

    Make all the methods static.

--- src/main/java/hashtools/operation/OperationPerformer.java
+++ src/main/java/hashtools/operation/OperationPerformer.java
@@ -16,26 +16,27 @@ public class OperationPerformer {
     private static final List<Condition> NO_CONDITION = List.of(new NoCondition());
     private static final Operation NO_OPERATION = new NoOperation();
 
+    @Deprecated(forRemoval = true)
     private ExecutorService threadPool;
 
 
-    public void perform(Operation operation) {
+    public static void perform(Operation operation) {
         perform(NO_CONDITION, operation, NO_OPERATION);
     }
 
-    public void perform(Condition condition, Operation operation) {
+    public static void perform(Condition condition, Operation operation) {
         perform(List.of(condition), operation, NO_OPERATION);
     }
 
-    public void perform(Collection<Condition> conditions, Operation operationIfAllTrue) {
+    public static void perform(Collection<Condition> conditions, Operation operationIfAllTrue) {
         perform(conditions, operationIfAllTrue, NO_OPERATION);
     }
 
-    public void perform(Condition condition, Operation operationIfTrue, Operation operationIfFalse) {
+    public static void perform(Condition condition, Operation operationIfTrue, Operation operationIfFalse) {
         perform(List.of(condition), operationIfTrue, operationIfFalse);
     }
 
-    public void perform(Collection<Condition> conditions, Operation operationIfAllTrue, Operation operationIfSomeFalse) {
+    public static void perform(Collection<Condition> conditions, Operation operationIfAllTrue, Operation operationIfSomeFalse) {
         if (Condition.allOf(conditions).isTrue()) {
             operationIfAllTrue.perform();
         } else {
@@ -43,22 +44,58 @@ public class OperationPerformer {
         }
     }
 
+
+    public static void performAsync(ExecutorService threadPool, Operation operation) {
+        performAsync(threadPool, NO_CONDITION, operation, NO_OPERATION);
+    }
+
+    public static void performAsync(ExecutorService threadPool, Condition condition, Operation operation) {
+        performAsync(threadPool, List.of(condition), operation, NO_OPERATION);
+    }
+
+    public static void performAsync(ExecutorService threadPool, Collection<Condition> conditions, Operation operationIfAllTrue) {
+        performAsync(threadPool, conditions, operationIfAllTrue, NO_OPERATION);
+    }
+
+    public static void performAsync(ExecutorService threadPool, Condition condition, Operation operationIfTrue, Operation operationIfFalse) {
+        performAsync(threadPool, List.of(condition), operationIfTrue, operationIfFalse);
+    }
+
+    public static void performAsync(ExecutorService threadPool, Collection<Condition> conditions, Operation operationIfAllTrue, Operation operationIfSomeFalse) {
+        try {
+            threadPool.execute(Condition.allOf(conditions).isTrue()
+                ? operationIfAllTrue::perform
+                : operationIfSomeFalse::perform
+            );
+        } catch (NullPointerException e) {
+            throw new UnsupportedOperationException("Thread pool not provided", e);
+        } catch (Exception e) {
+            throw new RuntimeException("Unknown issue", e);
+        }
+    }
+
+
+    @Deprecated(forRemoval = true)
     public void performAsync(Operation operation) {
         performAsync(NO_CONDITION, operation, NO_OPERATION);
     }
 
+    @Deprecated(forRemoval = true)
     public void performAsync(Condition condition, Operation operation) {
         performAsync(List.of(condition), operation, NO_OPERATION);
     }
 
+    @Deprecated(forRemoval = true)
     public void performAsync(Collection<Condition> conditions, Operation operationIfAllTrue) {
         performAsync(conditions, operationIfAllTrue, NO_OPERATION);
     }
 
+    @Deprecated(forRemoval = true)
     public void performAsync(Condition condition, Operation operationIfTrue, Operation operationIfFalse) {
         performAsync(List.of(condition), operationIfTrue, operationIfFalse);
     }
 
+    @Deprecated(forRemoval = true)
     public void performAsync(Collection<Condition> conditions, Operation operationIfAllTrue, Operation operationIfSomeFalse) {
         try {
             threadPool.execute(Condition.allOf(conditions).isTrue()
