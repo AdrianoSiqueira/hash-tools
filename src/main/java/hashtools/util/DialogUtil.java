3af1839f3469b49d44592eddecac2371eca5ddd4
adriano <as.adriano.siqueira@gmail.com>
2024-10-11 21:55:40 -0300

    Add 'DialogUtil' class.

---
+++ src/main/java/hashtools/util/DialogUtil.java
@@ -0,0 +1,60 @@
+package hashtools.util;
+
+import hashtools.condition.ThreadIsNotJavaFx;
+import hashtools.operation.OperationPerformer;
+import hashtools.operation.ThrowException;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.function.Function;
+
+public class DialogUtil {
+
+    private static FileChooser createFileChooser(String title, String initialDirectory) {
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle(title);
+        chooser.setInitialDirectory(new File(initialDirectory));
+        return chooser;
+    }
+
+    public static Optional<Path> showOpenDialog(String title, String initialDirectory, Collection<FileChooser.ExtensionFilter> filters, Window ownerWindow) {
+        OperationPerformer.perform(
+            new ThreadIsNotJavaFx(),
+            new ThrowException(new IllegalStateException("This method must be called within a JavaFX thread"))
+        );
+
+        FileChooser chooser = createFileChooser(title, initialDirectory);
+        chooser.getExtensionFilters().setAll(filters);
+
+        return Optional
+            .ofNullable(chooser.showOpenDialog(ownerWindow))
+            .map(new FileToAbsolutePath());
+    }
+
+    public static Optional<Path> showSaveDialog(String title, String initialDirectory, Window ownerWindow) {
+        OperationPerformer.perform(
+            new ThreadIsNotJavaFx(),
+            new ThrowException(new IllegalStateException("This method must be called within a JavaFX thread"))
+        );
+
+        FileChooser chooser = createFileChooser(title, initialDirectory);
+
+        return Optional
+            .ofNullable(chooser.showSaveDialog(ownerWindow))
+            .map(new FileToAbsolutePath());
+    }
+
+
+    private static final class FileToAbsolutePath implements Function<File, Path> {
+        @Override
+        public Path apply(File file) {
+            return file
+                .toPath()
+                .toAbsolutePath();
+        }
+    }
+}
