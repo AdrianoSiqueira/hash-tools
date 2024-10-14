6932cebd19fc9c0907fa5d328ead25a71f66899f
adriano <as.adriano.siqueira@gmail.com>
2024-10-13 23:03:16 -0300

    Remove 'translate' method.

--- src/main/java/hashtools/controller/Controller.java
+++ src/main/java/hashtools/controller/Controller.java
@@ -3,9 +3,6 @@ package hashtools.controller;
 import javafx.fxml.Initializable;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.MissingResourceException;
-import java.util.ResourceBundle;
-
 @Slf4j
 public abstract class Controller implements Initializable {
 
@@ -16,30 +13,4 @@ public abstract class Controller implements Initializable {
      * </p>
      */
     protected abstract void resetUI();
-
-    /**
-     * <p>
-     * Gets translation for the given dictionary entry. If
-     * some issue occurs, the entry itself will be returned.
-     * </p>
-     *
-     * @param dictionary Dictionary used to get translation.
-     * @param entry      Dictionary entry to get translation.
-     *
-     * @return The translation for the given entry, or the
-     * entry itself if translation fails.
-     */
-    protected final String translate(ResourceBundle dictionary, String entry) {
-        try {
-            return dictionary.getString(entry);
-        } catch (NullPointerException e) {
-            log.error("The dictionary or entry is null", e);
-        } catch (MissingResourceException e) {
-            log.error("The entry was not found in dictionary", e);
-        } catch (Exception e) {
-            log.error("Unknown issue", e);
-        }
-
-        return entry;
-    }
 }
