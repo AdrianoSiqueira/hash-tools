48d150b925c21fb06e6ed1439cf23f0b621ddd31
adriano <as.adriano.siqueira@gmail.com>
2024-10-13 22:48:05 -0300

    Add 'DisableNode' class.

---
+++ src/main/java/hashtools/operation/DisableNode.java
@@ -0,0 +1,20 @@
+package hashtools.operation;
+
+
+import hashtools.domain.Resource;
+import javafx.scene.Node;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class DisableNode implements Operation {
+
+    private final Node node;
+
+    @Override
+    public void perform() {
+        node.pseudoClassStateChanged(
+            Resource.Static.DISABLED,
+            true
+        );
+    }
+}
