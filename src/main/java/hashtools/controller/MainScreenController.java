546aa20cdfe36ece070c6352dab30a67a284bcaf
adriano <as.adriano.siqueira@gmail.com>
2024-10-15 21:19:49 -0300

    Remove 'OpenGeneratorScreen' inner class.

--- src/main/java/hashtools/controller/MainScreenController.java
+++ src/main/java/hashtools/controller/MainScreenController.java
@@ -124,15 +124,6 @@ public class MainScreenController implements Initializable, NotificationReceiver
     }
 
 
-    private final class OpenGeneratorScreen implements Operation {
-        @Override
-        public void perform() {
-            // TODO Remove this statement when the generator screen is done
-            System.out.println(getClass().getSimpleName());
-            pnlGenerator.setVisible(true);
-        }
-    }
-
     private final class OpenMainMenu implements Operation {
         @Override
         public void perform() {
