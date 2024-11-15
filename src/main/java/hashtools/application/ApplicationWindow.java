9c6803f50013cc868dede9cba8bcf2bea3bbbbdd
adriano <as.adriano.siqueira@gmail.com>
2024-11-15 15:20:04 -0300

    Split 'start' method into 'createScene' and 'loadFavIcon' methods.

--- src/main/java/hashtools/application/ApplicationWindow.java
+++ src/main/java/hashtools/application/ApplicationWindow.java
@@ -1,7 +1,6 @@
 package hashtools.application;
 
 import hashtools.preloader.PreloaderWindow;
-import hashtools.shared.Resource;
 import javafx.application.Application;
 import javafx.collections.ObservableMap;
 import javafx.fxml.FXMLLoader;
@@ -11,51 +10,69 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
 import static hashtools.shared.Resource.ApplicationDimension.HEIGHT;
 import static hashtools.shared.Resource.ApplicationDimension.WIDTH;
+import static hashtools.shared.Resource.FXMLPath.APPLICATION_SCREEN;
 import static hashtools.shared.Resource.ImagePath.FAV_ICON;
+import static hashtools.shared.Resource.ResourceBundle.LANGUAGE;
 
 @Slf4j
 public class ApplicationWindow extends Application {
 
+    private Scene createScene()
+    throws IOException {
+        URL fxmlPath = Optional
+            .ofNullable(getClass().getResource(APPLICATION_SCREEN))
+            .orElseThrow(() -> new FileNotFoundException(APPLICATION_SCREEN));
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(fxmlPath);
+        loader.setResources(ResourceBundle.getBundle(LANGUAGE));
+
+        /*
+         * Namespace is used to define constants that
+         * can be accessed in the fxml file this loader
+         * is working with.
+         */
+        ObservableMap<String, Object> namespace = loader.getNamespace();
+        namespace.put("applicationWidth", WIDTH);
+        namespace.put("applicationHeight", HEIGHT);
+
+        Pane pane = loader.load();
+        return new Scene(pane);
+    }
+
+    private Image loadFavIcon()
+    throws FileNotFoundException {
+        return Optional
+            .ofNullable(getClass().getResourceAsStream(FAV_ICON))
+            .map(Image::new)
+            .orElseThrow(() -> new FileNotFoundException(FAV_ICON));
+    }
+
     @Override
     public void start(Stage stage) {
         try {
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getResource(Resource.FXMLPath.APPLICATION_SCREEN));
-            loader.setResources(ResourceBundle.getBundle(Resource.ResourceBundle.LANGUAGE));
-
-            /*
-             * Namespace is used to define constants that
-             * can be accessed in the fxml file this loader
-             * is working with.
-             */
-            ObservableMap<String, Object> namespace = loader.getNamespace();
-            namespace.put("applicationWidth", WIDTH);
-            namespace.put("applicationHeight", HEIGHT);
-
-            Pane pane = loader.load();
-            stage.setScene(new Scene(pane));
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to load the fxml file.", e);
+            stage.getIcons().add(loadFavIcon());
+        } catch (FileNotFoundException e) {
+            log.error("Failed to load fav icon", e);
         }
 
-        Optional
-            .ofNullable(getClass().getResourceAsStream(FAV_ICON))
-            .map(Image::new)
-            .ifPresentOrElse(
-                stage.getIcons()::add,
-                () -> log.error("Failed to load fav icon")
-            );
-
-        stage.setTitle("HashTools");
-        stage.sizeToScene();
-        stage.centerOnScreen();
-        stage.show();
+        try {
+            stage.setScene(createScene());
+            stage.setTitle("HashTools");
+            stage.sizeToScene();
+            stage.centerOnScreen();
+            stage.show();
+        } catch (Exception e) {
+            log.error("Failed to create scene", e);
+        }
 
         notifyPreloader(new PreloaderWindow.ClosePreloaderNotification());
     }
--- src/main/java/hashtools/preloader/PreloaderWindow.java
+++ src/main/java/hashtools/preloader/PreloaderWindow.java
@@ -1,6 +1,5 @@
 package hashtools.preloader;
 
-import hashtools.shared.Resource;
 import javafx.application.Preloader;
 import javafx.collections.ObservableMap;
 import javafx.fxml.FXMLLoader;
@@ -8,16 +7,46 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import lombok.extern.slf4j.Slf4j;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import static hashtools.shared.Resource.ApplicationDimension.HEIGHT;
 import static hashtools.shared.Resource.ApplicationDimension.WIDTH;
+import static hashtools.shared.Resource.FXMLPath.PRELOADER_SCREEN;
+import static hashtools.shared.Resource.ResourceBundle.LANGUAGE;
 
+@Slf4j
 public class PreloaderWindow extends Preloader {
 
     private Stage stage;
+
+    private Scene createScene()
+    throws IOException {
+        URL fxmlPath = Optional
+            .ofNullable(getClass().getResource(PRELOADER_SCREEN))
+            .orElseThrow(() -> new FileNotFoundException(PRELOADER_SCREEN));
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(fxmlPath);
+        loader.setResources(ResourceBundle.getBundle(LANGUAGE));
+
+        /*
+         * Namespace is used to define constants that
+         * can be accessed in the fxml file this loader
+         * is working with.
+         */
+        ObservableMap<String, Object> namespace = loader.getNamespace();
+        namespace.put("applicationWidth", WIDTH);
+        namespace.put("applicationHeight", HEIGHT);
+
+        Pane pane = loader.load();
+        return new Scene(pane);
+    }
 
     @Override
     public void handleApplicationNotification(PreloaderNotification notification) {
@@ -28,31 +57,17 @@ public class PreloaderWindow extends Preloader {
 
     @Override
     public void start(Stage stage) {
-        try {
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getResource(Resource.FXMLPath.PRELOADER_SCREEN));
-            loader.setResources(ResourceBundle.getBundle(Resource.ResourceBundle.LANGUAGE));
-
-            /*
-             * Namespace is used to define constants that
-             * can be accessed in the fxml file this loader
-             * is working with.
-             */
-            ObservableMap<String, Object> namespace = loader.getNamespace();
-            namespace.put("applicationWidth", WIDTH);
-            namespace.put("applicationHeight", HEIGHT);
-
-            Pane pane = loader.load();
-            stage.setScene(new Scene(pane));
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to load fxml file", e);
-        }
-
         this.stage = stage;
-        stage.initStyle(StageStyle.UNDECORATED);
-        stage.sizeToScene();
-        stage.centerOnScreen();
-        stage.show();
+
+        try {
+            stage.setScene(createScene());
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.sizeToScene();
+            stage.centerOnScreen();
+            stage.show();
+        } catch (IOException e) {
+            log.error("Failed to create scene", e);
+        }
     }
 
     public static final class ClosePreloaderNotification implements PreloaderNotification {}
