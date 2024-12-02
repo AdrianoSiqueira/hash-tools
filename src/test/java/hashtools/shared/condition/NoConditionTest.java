703a281adbdb69ea033f5dc25b67738c09815953
adriano <as.adriano.siqueira@gmail.com>
2024-12-01 21:56:32 -0300

    Add tests for 'NoCondition'.

---
+++ src/test/java/hashtools/shared/condition/NoConditionTest.java
@@ -0,0 +1,23 @@
+package hashtools.shared.condition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class NoConditionTest {
+
+    @Test
+    void isFalseAlwaysReturnsFalse() {
+        assertFalse(
+            () -> new NoCondition().isFalse()
+        );
+    }
+
+    @Test
+    void isTrueAlwaysReturnsTrue() {
+        assertTrue(
+            () -> new NoCondition().isTrue()
+        );
+    }
+}
