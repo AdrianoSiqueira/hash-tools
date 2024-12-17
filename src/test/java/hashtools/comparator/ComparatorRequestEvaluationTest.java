6adbb2966e628014642b00de1a32c2f62f15ae57
adriano <as.adriano.siqueira@gmail.com>
2024-12-16 21:21:52 -0300

    Merge 'evaluate' methods.

--- src/test/java/hashtools/comparator/ComparatorRequestEvaluationTest.java
+++ src/test/java/hashtools/comparator/ComparatorRequestEvaluationTest.java
@@ -4,7 +4,7 @@ import hashtools.comparator.exception.MissingInputFile1Exception;
 import hashtools.comparator.exception.MissingInputFile2Exception;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -16,42 +16,49 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-// TODO Check when file is folder
 class ComparatorRequestEvaluationTest {
 
     @SuppressWarnings("FieldCanBeLocal")
     private static Path
         nullFile,
         nonExistentFile,
+        folder,
         validFile;
 
     private static ComparatorRequest
         nullRequest,
-        nullFile1,
-        missingFile1,
-        nullFile2,
-        missingFile2,
+        nullFile1Request,
+        missingFile1Request,
+        folderFile1Request,
+        nullFile2Request,
+        missingFile2Request,
+        folderFile2Request,
         validRequest;
 
 
     @AfterAll
     static void cleanup() throws Exception {
+        Files.deleteIfExists(folder);
         Files.deleteIfExists(validFile);
     }
 
     static Stream<Arguments> getTests() {
         return Stream.of(
-            Arguments.of(NullPointerException.class, nullRequest),
-            Arguments.of(MissingInputFile1Exception.class, nullFile1),
-            Arguments.of(MissingInputFile1Exception.class, missingFile1),
-            Arguments.of(MissingInputFile2Exception.class, nullFile2),
-            Arguments.of(MissingInputFile2Exception.class, missingFile2)
+            Arguments.of(true, NullPointerException.class, nullRequest),
+            Arguments.of(true, MissingInputFile1Exception.class, nullFile1Request),
+            Arguments.of(true, MissingInputFile1Exception.class, missingFile1Request),
+            Arguments.of(true, MissingInputFile1Exception.class, folderFile1Request),
+            Arguments.of(true, MissingInputFile2Exception.class, nullFile2Request),
+            Arguments.of(true, MissingInputFile2Exception.class, missingFile2Request),
+            Arguments.of(true, MissingInputFile2Exception.class, folderFile2Request),
+            Arguments.of(false, null, validRequest)
         );
     }
 
     @BeforeAll
     static void setup() throws Exception {
         nullFile = null;
+        folder = Files.createTempDirectory("_folder_");
         validFile = Files.createTempFile("_valid_", "");
 
         nonExistentFile = Files.createTempFile("_non-existent_", "");
@@ -60,21 +67,29 @@ class ComparatorRequestEvaluationTest {
 
         nullRequest = null;
 
-        nullFile1 = new ComparatorRequest();
-        nullFile1.setInputFile1(nullFile);
-        nullFile1.setInputFile2(validFile);
+        nullFile1Request = new ComparatorRequest();
+        nullFile1Request.setInputFile1(nullFile);
+        nullFile1Request.setInputFile2(validFile);
 
-        missingFile1 = new ComparatorRequest();
-        missingFile1.setInputFile1(nonExistentFile);
-        missingFile1.setInputFile2(validFile);
+        missingFile1Request = new ComparatorRequest();
+        missingFile1Request.setInputFile1(nonExistentFile);
+        missingFile1Request.setInputFile2(validFile);
 
-        nullFile2 = new ComparatorRequest();
-        nullFile2.setInputFile1(validFile);
-        nullFile2.setInputFile2(nullFile);
+        folderFile1Request = new ComparatorRequest();
+        folderFile1Request.setInputFile1(folder);
+        folderFile1Request.setInputFile2(validFile);
 
-        missingFile2 = new ComparatorRequest();
-        missingFile2.setInputFile1(validFile);
-        missingFile2.setInputFile2(nonExistentFile);
+        nullFile2Request = new ComparatorRequest();
+        nullFile2Request.setInputFile1(validFile);
+        nullFile2Request.setInputFile2(nullFile);
+
+        missingFile2Request = new ComparatorRequest();
+        missingFile2Request.setInputFile1(validFile);
+        missingFile2Request.setInputFile2(nonExistentFile);
+
+        folderFile2Request = new ComparatorRequest();
+        folderFile2Request.setInputFile1(validFile);
+        folderFile2Request.setInputFile2(folder);
 
         validRequest = new ComparatorRequest();
         validRequest.setInputFile1(validFile);
@@ -84,17 +99,13 @@ class ComparatorRequestEvaluationTest {
 
     @ParameterizedTest
     @MethodSource(value = "getTests")
-    void evaluate(Class<? extends Throwable> expected, ComparatorRequest request) {
-        assertThrows(
-            expected,
-            () -> new ComparatorRequestEvaluation(request).evaluate()
-        );
-    }
+    void evaluate(boolean shouldThrow, Class<? extends Throwable> expected, ComparatorRequest request) {
+        Executable executable = () -> new ComparatorRequestEvaluation(request).evaluate();
 
-    @Test
-    void evaluateDoesNotThrowExceptionWhenRequestIsValid() {
-        assertDoesNotThrow(
-            () -> new ComparatorRequestEvaluation(validRequest).evaluate()
-        );
+        if (shouldThrow) {
+            assertThrows(expected, executable);
+        } else {
+            assertDoesNotThrow(executable);
+        }
     }
 }
