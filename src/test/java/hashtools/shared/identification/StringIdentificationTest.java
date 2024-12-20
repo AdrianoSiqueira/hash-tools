018577451e16df26bf92bd12fc9cfc82eb8bdb8d
adriano <as.adriano.siqueira@gmail.com>
2024-12-19 21:04:43 -0300

    Implement parameterized tests.

--- src/test/java/hashtools/shared/identification/StringIdentificationTest.java
+++ src/test/java/hashtools/shared/identification/StringIdentificationTest.java
@@ -1,45 +1,44 @@
 package hashtools.shared.identification;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StringIdentificationTest {
 
-    private static String nullString;
-    private static String emptyString;
-    private static String string;
+    private static String
+        nullString,
+        emptyString,
+        regularString;
 
 
-    @BeforeAll
-    static void createStrings() {
-        nullString = null;
-        emptyString = "";
-        string = "string";
-    }
-
-    @Test
-    void identifyReturnsTheStringItselfWhenStringIsEmpty() {
-        assertEquals(
-            emptyString,
-            new StringIdentification(emptyString).identity()
+    static Stream<Arguments> getTests() {
+        return Stream.of(
+            Arguments.of(nullString),
+            Arguments.of(emptyString),
+            Arguments.of(regularString)
         );
     }
 
-    @Test
-    void identifyReturnsTheStringItselfWhenStringIsNotEmpty() {
+    @BeforeAll
+    static void setup() {
+        nullString = null;
+        emptyString = "";
+        regularString = "string";
+    }
+
+
+    @ParameterizedTest
+    @MethodSource(value = "getTests")
+    void identify(String string) {
         assertEquals(
             string,
             new StringIdentification(string).identity()
-        );
-    }
-
-    @Test
-    void identifyReturnsTheStringItselfWhenStringIsNull() {
-        assertEquals(
-            nullString,
-            new StringIdentification(nullString).identity()
         );
     }
 }
