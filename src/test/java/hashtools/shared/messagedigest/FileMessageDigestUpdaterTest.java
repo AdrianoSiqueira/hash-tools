5ea91b546902e868e6b307b8928666495c9726be
adriano <as.adriano.siqueira@gmail.com>
2024-12-19 21:15:35 -0300

    Implement parameterized tests.

--- src/test/java/hashtools/shared/messagedigest/FileMessageDigestUpdaterTest.java
+++ src/test/java/hashtools/shared/messagedigest/FileMessageDigestUpdaterTest.java
@@ -2,102 +2,76 @@ package hashtools.shared.messagedigest;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class FileMessageDigestUpdaterTest {
 
-    private static Path nullFile;
-    private static Path emptyFile;
-    private static Path file;
-    private static Path folder;
-    private static Path nonExistentFile;
+    private static Path
+        nullFile,
+        noPathFile,
+        regularFile,
+        folder,
+        nonExistentFile;
 
-    private static MessageDigest messageDigest;
+    private static MessageDigest
+        nullMessageDigest,
+        messageDigest;
 
-
-    @BeforeAll
-    static void createFiles() {
-        try {
-            nullFile = null;
-            emptyFile = Path.of("");
-            folder = Files.createTempDirectory("");
-
-            file = Files.createTempFile("", "");
-            Files.writeString(file, "a");
-
-            nonExistentFile = Files.createTempFile("", "");
-            Files.deleteIfExists(nonExistentFile);
-
-            messageDigest = MessageDigest.getInstance("MD5");
-        } catch (IOException | NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     @AfterAll
-    static void removeFiles() {
-        try {
-            Files.deleteIfExists(file);
-            Files.deleteIfExists(folder);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    static void cleanup() throws Exception {
+        Files.deleteIfExists(regularFile);
+        Files.deleteIfExists(folder);
+    }
+
+    static Stream<Arguments> getTests() {
+        return Stream.of(
+            Arguments.of(true, NullPointerException.class, nullMessageDigest, regularFile),
+            Arguments.of(true, NullPointerException.class, messageDigest, nullFile),
+            Arguments.of(true, RuntimeException.class, messageDigest, noPathFile),
+            Arguments.of(true, RuntimeException.class, messageDigest, folder),
+            Arguments.of(true, RuntimeException.class, messageDigest, nonExistentFile),
+            Arguments.of(false, null, messageDigest, regularFile)
+        );
+    }
+
+    @BeforeAll
+    static void setup() throws Exception {
+        nullFile = null;
+        noPathFile = Path.of("");
+        folder = Files.createTempDirectory("_folder_");
+
+        regularFile = Files.createTempFile("_file_", "");
+        Files.writeString(regularFile, "a");
+
+        nonExistentFile = Files.createTempFile("", "");
+        Files.deleteIfExists(nonExistentFile);
+
+        nullMessageDigest = null;
+        messageDigest = MessageDigest.getInstance("MD5");
+    }
+
+
+    @ParameterizedTest
+    @MethodSource(value = "getTests")
+    void update(boolean shouldThrow, Class<? extends Throwable> expectedException, MessageDigest messageDigest, Path file) {
+        Executable executable = () -> new FileMessageDigestUpdater(file).update(messageDigest);
+
+        if (shouldThrow) {
+            assertThrows(expectedException, executable);
+        } else {
+            assertDoesNotThrow(executable);
         }
-    }
-
-
-    @Test
-    void updateDoesNotThrowExceptionWhenFileIsFile() {
-        assertDoesNotThrow(
-            () -> new FileMessageDigestUpdater(file).update(messageDigest)
-        );
-    }
-
-    @Test
-    void updateThrowsNullPointerExceptionWhenFileIsNull() {
-        assertThrows(
-            NullPointerException.class,
-            () -> new FileMessageDigestUpdater(nullFile).update(messageDigest)
-        );
-    }
-
-    @Test
-    void updateThrowsNullPointerExceptionWhenMessageDigestIsNull() {
-        assertThrows(
-            NullPointerException.class,
-            () -> new FileMessageDigestUpdater(file).update(null)
-        );
-    }
-
-    @Test
-    void updateThrowsRuntimeExceptionWhenFileDoesNotExist() {
-        assertThrows(
-            RuntimeException.class,
-            () -> new FileMessageDigestUpdater(nonExistentFile).update(messageDigest)
-        );
-    }
-
-    @Test
-    void updateThrowsRuntimeExceptionWhenFileIsEmpty() {
-        assertThrows(
-            RuntimeException.class,
-            () -> new FileMessageDigestUpdater(emptyFile).update(messageDigest)
-        );
-    }
-
-    @Test
-    void updateThrowsRuntimeExceptionWhenFileIsFolder() {
-        assertThrows(
-            RuntimeException.class,
-            () -> new FileMessageDigestUpdater(folder).update(messageDigest)
-        );
     }
 }
